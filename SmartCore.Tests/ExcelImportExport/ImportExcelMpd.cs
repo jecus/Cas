9ac95318ec41f5b639d2223ea7ea9db722f1857a
Jenya Jecus <evgenii.babak@outlook.com>
2019-02-06 18:51:48 +0300

    export CRJ

--- SmartCore.Tests/ExcelImportExport/ImportExcelMpd.cs
+++ SmartCore.Tests/ExcelImportExport/ImportExcelMpd.cs
@@ -32,16 +32,13 @@ namespace SmartCore.Tests.ExcelImportExport
 			var ds = ExcelToDataTableUsingExcelDataReader(@"D:\123\mpd.xlsx");
 
 			aircraftCore.LoadAllAircrafts();
-			var aircraft = aircraftCore.GetAircraftById(2330);
+			var aircraft = aircraftCore.GetAircraftById(2334);
 
 			var bd = componentCore.GetAicraftBaseComponents(aircraft.ItemId, BaseComponentType.Frame.ItemId).FirstOrDefault();
 			var ata = env.NewLoader.GetObjectListAll<ATAChapterDTO, AtaChapter>();
 
-
-
 			var mpds = mpdCore.GetMaintenanceDirectives(aircraft);
 
-			MaintenanceDirective savedMpd = null;
 
 			foreach (DataTable table in ds.Tables)
 			{
@@ -176,11 +173,111 @@ namespace SmartCore.Tests.ExcelImportExport
 			}
 		}
 
+		[TestMethod]
+		public void ImportMaintenanceDirectivesCRJ()
+		{
+			var env = GetEnviroment();
+
+			var aircraftCore = new AircraftsCore(env.Loader, env.NewKeeper, env.NewLoader);
+			var itemRelationCore = new ItemsRelationsDataAccess(env);
+			var componentCore = new ComponentCore(env, env.Loader, env.NewLoader, env.NewKeeper, aircraftCore, itemRelationCore);
+			var mpdCore = new MaintenanceCore(env, env.NewLoader, env.NewKeeper, itemRelationCore, aircraftCore);
+
+			var ds = ExcelToDataTableUsingExcelDataReader(@"D:\123\mpd.xlsx");
+
+			aircraftCore.LoadAllAircrafts();
+			var aircraft = aircraftCore.GetAircraftById(233);
+
+			var bd = componentCore.GetAicraftBaseComponents(aircraft.ItemId, BaseComponentType.Frame.ItemId).FirstOrDefault();
+			var ata = env.NewLoader.GetObjectListAll<ATAChapterDTO, AtaChapter>();
+
+			var mpds = mpdCore.GetMaintenanceDirectives(aircraft);
+
+			foreach (DataTable table in ds.Tables)
+			{
+				foreach (DataRow row in table.Rows)
+				{
+					var find = mpds.FirstOrDefault(i => i.TaskNumberCheck.ToLower().Equals(row[1].ToString().ToLower()));
+
+					MaintenanceDirective mpd;
+
+					if (find != null)
+						mpd = find;
+					else
+						mpd = new MaintenanceDirective()
+						{
+							ParentBaseComponent = bd,
+							HiddenRemarks = "NEW",
+						};
+
+					//Section 2.2
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsAndPowerPlants;
+					//mpd.MpdRef = "Section 2.2";
+
+					//Section 2.3
+					//mpd.Program = MaintenanceDirectiveProgramType.StructuralInspection;
+					//mpd.MpdRef = "Section 2.3";
+
+					//Section 2.3
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.3";
+
+					//Section 2.4
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.4";
+
+					//Section 2.5
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.5";
+
+					//Section 2.6
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.6";
+
+					//Section 2.7
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.7";
+
+					//Section 2.8
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.8";
+
+					//Section 2.9
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.9";
+
+					//Section 2.10
+					//mpd.Program = MaintenanceDirectiveProgramType.SystemsMaintenance;
+					//mpd.MpdRef = "Section 2.10";
+
+					mpd.TaskNumberCheck = row[1].ToString();
+					mpd.TaskCardNumber = row[2].ToString();
+					mpd.MaintenanceManual = row[3].ToString();
+
+					if (mpd.TaskNumberCheck.Length > 2)
+					{
+						var shortName = mpd.TaskNumberCheck.Substring(0, 1);
+						mpd.ATAChapter = ata.FirstOrDefault(a => a.ShortName.Equals(shortName));
+					}
+
+					mpd.MPDTaskNumber = "SCP A-054-000";
+					mpd.IsOperatorTask = false;
+					mpd.MpdRevisionDate = new DateTime(2018, 1, 22);
+					mpd.Threshold.EffectiveDate = new DateTime(2018, 10, 10);
+					mpd.ScheduleRevisionDate = new DateTime(2018, 10, 10);
+					mpd.ScheduleRevisionNum = "0";
+					mpd.MpdRevisionNum = "38";
+					mpd.ScheduleRef = "MP SCAT/CRJ/AMP/I4/R00";
+
+				}
+			}
+		}
+
 
 		private CasEnvironment GetEnviroment()
 		{
 			var cas = new CasEnvironment();
-			cas.Connect("91.213.233.139:45617", "casadmin", "casadmin001", "ScatDB");
+			cas.Connect("92.47.31.254:45617", "casadmin", "casadmin001", "ScatDB");
 			DbTypes.CasEnvironment = cas;
 			cas.NewLoader.FirstLoad();
 
