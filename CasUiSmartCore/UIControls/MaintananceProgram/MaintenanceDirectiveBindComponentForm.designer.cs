6a8ba49680ae1d76cfce02418f217737a09ee01c
Misha <Misha@DESKTOP-2KBGUT9>
2019-04-18 22:02:16 +0600

    MaintenanceDirectiveBindComponentForm - Metro

--- CasUiSmartCore/UIControls/MaintananceProgram/MaintenanceDirectiveBindComponentForm.designer.cs
+++ CasUiSmartCore/UIControls/MaintananceProgram/MaintenanceDirectiveBindComponentForm.designer.cs
@@ -33,11 +33,12 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.buttonAdd = new System.Windows.Forms.Button();
 			this.buttonClose = new System.Windows.Forms.Button();
 			this.splitContainerMain = new System.Windows.Forms.SplitContainer();
-			this.comboBoxRelationType = new System.Windows.Forms.ComboBox();
-			this.buttonDelete = new System.Windows.Forms.Button();
 			this.listViewTasksForSelect = new CAS.UI.UIControls.MaintananceProgram.MaintenanceDirectiveBindComponentListView();
 			this.listViewBindedTasks = new CAS.UI.UIControls.MaintananceProgram.MaintenanceDirectiveBindComponentListView();
+			this.comboBoxRelationType = new System.Windows.Forms.ComboBox();
+			this.buttonDelete = new System.Windows.Forms.Button();
 			this.buttonOk = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
 			this.splitContainerMain.Panel1.SuspendLayout();
 			this.splitContainerMain.Panel2.SuspendLayout();
 			this.splitContainerMain.SuspendLayout();
@@ -49,7 +50,7 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonAdd.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.buttonAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonAdd.Location = new System.Drawing.Point(769, 684);
+			this.buttonAdd.Location = new System.Drawing.Point(769, 738);
 			this.buttonAdd.Name = "buttonAdd";
 			this.buttonAdd.Size = new System.Drawing.Size(144, 33);
 			this.buttonAdd.TabIndex = 12;
@@ -63,7 +64,7 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonClose.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.buttonClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonClose.Location = new System.Drawing.Point(919, 684);
+			this.buttonClose.Location = new System.Drawing.Point(919, 738);
 			this.buttonClose.Name = "buttonClose";
 			this.buttonClose.Size = new System.Drawing.Size(75, 33);
 			this.buttonClose.TabIndex = 13;
@@ -75,7 +76,7 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.splitContainerMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.splitContainerMain.Location = new System.Drawing.Point(15, 12);
+			this.splitContainerMain.Location = new System.Drawing.Point(15, 63);
 			this.splitContainerMain.Name = "splitContainerMain";
 			this.splitContainerMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
@@ -88,33 +89,8 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.splitContainerMain.Panel2.AutoScroll = true;
 			this.splitContainerMain.Panel2.Controls.Add(this.listViewBindedTasks);
 			this.splitContainerMain.Size = new System.Drawing.Size(979, 666);
-			this.splitContainerMain.SplitterDistance = 378;
+			this.splitContainerMain.SplitterDistance = 392;
 			this.splitContainerMain.TabIndex = 14;
-			// 
-			// comboBoxRelationType
-			// 
-			this.comboBoxRelationType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.comboBoxRelationType.BackColor = System.Drawing.Color.White;
-			this.comboBoxRelationType.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-			this.comboBoxRelationType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-			this.comboBoxRelationType.FormattingEnabled = true;
-			this.comboBoxRelationType.Location = new System.Drawing.Point(385, 691);
-			this.comboBoxRelationType.Name = "comboBoxRelationType";
-			this.comboBoxRelationType.Size = new System.Drawing.Size(196, 22);
-			this.comboBoxRelationType.TabIndex = 16;
-			// 
-			// buttonDelete
-			// 
-			this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonDelete.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-			this.buttonDelete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonDelete.Location = new System.Drawing.Point(678, 684);
-			this.buttonDelete.Name = "buttonDelete";
-			this.buttonDelete.Size = new System.Drawing.Size(85, 33);
-			this.buttonDelete.TabIndex = 15;
-			this.buttonDelete.Text = "Delete";
-			this.buttonDelete.Click += new System.EventHandler(this.ButtonDeleteClick);
 			// 
 			// listViewTasksForSelect
 			// 
@@ -127,7 +103,7 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.listViewTasksForSelect.Name = "listViewTasksForSelect";
 			this.listViewTasksForSelect.ReflectionType = CAS.UI.Management.Dispatchering.ReflectionTypes.DisplayInCurrent;
 			this.listViewTasksForSelect.ShowGroups = true;
-			this.listViewTasksForSelect.Size = new System.Drawing.Size(979, 378);
+			this.listViewTasksForSelect.Size = new System.Drawing.Size(979, 392);
 			this.listViewTasksForSelect.TabIndex = 1;
 			this.listViewTasksForSelect.SelectedItemsChanged += new System.EventHandler<CAS.UI.UIControls.Auxiliary.SelectedItemsChangeEventArgs>(this.ListViewSelectedTasksSelectedItemsChanged);
 			// 
@@ -142,9 +118,34 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.listViewBindedTasks.Name = "listViewBindedTasks";
 			this.listViewBindedTasks.ReflectionType = CAS.UI.Management.Dispatchering.ReflectionTypes.DisplayInCurrent;
 			this.listViewBindedTasks.ShowGroups = true;
-			this.listViewBindedTasks.Size = new System.Drawing.Size(979, 284);
+			this.listViewBindedTasks.Size = new System.Drawing.Size(979, 270);
 			this.listViewBindedTasks.TabIndex = 2;
 			this.listViewBindedTasks.SelectedItemsChanged += new System.EventHandler<CAS.UI.UIControls.Auxiliary.SelectedItemsChangeEventArgs>(this.ListViewBindedTasksSelectedItemsChanged);
+			// 
+			// comboBoxRelationType
+			// 
+			this.comboBoxRelationType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBoxRelationType.BackColor = System.Drawing.Color.White;
+			this.comboBoxRelationType.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.comboBoxRelationType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
+			this.comboBoxRelationType.FormattingEnabled = true;
+			this.comboBoxRelationType.Location = new System.Drawing.Point(385, 745);
+			this.comboBoxRelationType.Name = "comboBoxRelationType";
+			this.comboBoxRelationType.Size = new System.Drawing.Size(196, 22);
+			this.comboBoxRelationType.TabIndex = 16;
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonDelete.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonDelete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
+			this.buttonDelete.Location = new System.Drawing.Point(678, 738);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(85, 33);
+			this.buttonDelete.TabIndex = 15;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.Click += new System.EventHandler(this.ButtonDeleteClick);
 			// 
 			// buttonOk
 			// 
@@ -152,18 +153,17 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonOk.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.buttonOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonOk.Location = new System.Drawing.Point(587, 684);
+			this.buttonOk.Location = new System.Drawing.Point(587, 738);
 			this.buttonOk.Name = "buttonOk";
 			this.buttonOk.Size = new System.Drawing.Size(85, 33);
 			this.buttonOk.TabIndex = 17;
 			this.buttonOk.Text = "Ok";
 			this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
 			// 
-			// MaintenanceDirectiveBindDetailForm
+			// MaintenanceDirectiveBindComponentForm
 			// 
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
 			this.CancelButton = this.buttonClose;
-			this.ClientSize = new System.Drawing.Size(1006, 723);
+			this.ClientSize = new System.Drawing.Size(1006, 777);
 			this.Controls.Add(this.buttonOk);
 			this.Controls.Add(this.comboBoxRelationType);
 			this.Controls.Add(this.buttonDelete);
@@ -174,13 +174,13 @@ namespace CAS.UI.UIControls.MaintananceProgram
 			this.MinimumSize = new System.Drawing.Size(860, 640);
 			this.Name = "MaintenanceDirectiveBindComponentForm";
 			this.ShowIcon = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Maintenance Directive Bind Components Form";
 			this.Activated += new System.EventHandler(this.TemplateAircraftAddToDataBaseForm_Activated);
 			this.Deactivate += new System.EventHandler(this.TemplateAircraftAddToDataBaseForm_Deactivate);
 			this.Load += new System.EventHandler(this.MaintenanceCheckBindTaskFormLoad);
 			this.splitContainerMain.Panel1.ResumeLayout(false);
 			this.splitContainerMain.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
 			this.splitContainerMain.ResumeLayout(false);
 			this.ResumeLayout(false);
 
