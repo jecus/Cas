9dc19579c718661856dae8cfac52dafe48c70832
Misha <Misha@DESKTOP-2KBGUT9>
2019-04-18 22:35:47 +0600

    ForecastCustomsMTOP - Metro

--- CasUiSmartCore/UIControls/ForecastControls/ForecastCustomsMTOP.Designer.cs
+++ CasUiSmartCore/UIControls/ForecastControls/ForecastCustomsMTOP.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using MetroFramework.Controls;
 
 namespace CAS.UI.UIControls.ForecastControls
 {
@@ -47,21 +48,21 @@ namespace CAS.UI.UIControls.ForecastControls
         {
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
-			this.labelHours = new System.Windows.Forms.Label();
+			this.labelHours = new MetroFramework.Controls.MetroLabel();
 			this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
 			this.tabPageCheck = new System.Windows.Forms.TabPage();
-			this.radioButtonZero = new System.Windows.Forms.RadioButton();
-			this.radioButtonAll = new System.Windows.Forms.RadioButton();
+			this.radioButtonZero = new MetroFramework.Controls.MetroRadioButton();
+			this.radioButtonAll = new MetroFramework.Controls.MetroRadioButton();
 			this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
-			this.labelTo = new System.Windows.Forms.Label();
+			this.labelTo = new MetroFramework.Controls.MetroLabel();
 			this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
-			this.labelPeriodFrom = new System.Windows.Forms.Label();
+			this.labelPeriodFrom = new MetroFramework.Controls.MetroLabel();
 			this.tabControlMain = new System.Windows.Forms.TabControl();
 			this.numericUpDownCycles = new System.Windows.Forms.NumericUpDown();
-			this.labelCycles = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
+			this.labelCycles = new MetroFramework.Controls.MetroLabel();
+			this.label1 = new MetroFramework.Controls.MetroLabel();
 			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new MetroFramework.Controls.MetroCheckBox();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
 			this.tabPageCheck.SuspendLayout();
 			this.tabControlMain.SuspendLayout();
@@ -72,9 +73,12 @@ namespace CAS.UI.UIControls.ForecastControls
 			// button1
 			// 
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(169, 241);
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
+			this.button1.Location = new System.Drawing.Point(169, 292);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(75, 33);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "OK";
 			this.button1.UseVisualStyleBackColor = true;
@@ -83,9 +87,12 @@ namespace CAS.UI.UIControls.ForecastControls
 			// button2
 			// 
 			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.button2.Location = new System.Drawing.Point(250, 241);
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
+			this.button2.Location = new System.Drawing.Point(250, 292);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.Size = new System.Drawing.Size(75, 33);
 			this.button2.TabIndex = 3;
 			this.button2.Text = "Cancel";
 			this.button2.UseVisualStyleBackColor = true;
@@ -94,11 +101,10 @@ namespace CAS.UI.UIControls.ForecastControls
 			// labelHours
 			// 
 			this.labelHours.AutoSize = true;
-			this.labelHours.Font = new System.Drawing.Font("Verdana", 11.25F);
 			this.labelHours.ForeColor = System.Drawing.Color.DimGray;
-			this.labelHours.Location = new System.Drawing.Point(11, 131);
+			this.labelHours.Location = new System.Drawing.Point(8, 187);
 			this.labelHours.Name = "labelHours";
-			this.labelHours.Size = new System.Drawing.Size(75, 18);
+			this.labelHours.Size = new System.Drawing.Size(62, 19);
 			this.labelHours.TabIndex = 42;
 			this.labelHours.Text = "HRS/DAY";
 			// 
@@ -112,7 +118,7 @@ namespace CAS.UI.UIControls.ForecastControls
             0,
             0,
             131072});
-			this.numericUpDownHours.Location = new System.Drawing.Point(92, 127);
+			this.numericUpDownHours.Location = new System.Drawing.Point(89, 187);
 			this.numericUpDownHours.Maximum = new decimal(new int[] {
             744,
             0,
@@ -149,29 +155,27 @@ namespace CAS.UI.UIControls.ForecastControls
 			// 
 			this.radioButtonZero.AutoSize = true;
 			this.radioButtonZero.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.radioButtonZero.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.radioButtonZero.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.radioButtonZero.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
 			this.radioButtonZero.Location = new System.Drawing.Point(147, 62);
 			this.radioButtonZero.Name = "radioButtonZero";
-			this.radioButtonZero.Size = new System.Drawing.Size(128, 18);
+			this.radioButtonZero.Size = new System.Drawing.Size(109, 15);
 			this.radioButtonZero.TabIndex = 29;
 			this.radioButtonZero.Text = "Zero Phase Only";
+			this.radioButtonZero.UseSelectable = true;
 			// 
 			// radioButtonAll
 			// 
 			this.radioButtonAll.AutoSize = true;
 			this.radioButtonAll.Checked = true;
 			this.radioButtonAll.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.radioButtonAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.radioButtonAll.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.radioButtonAll.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
 			this.radioButtonAll.Location = new System.Drawing.Point(60, 62);
 			this.radioButtonAll.Name = "radioButtonAll";
-			this.radioButtonAll.Size = new System.Drawing.Size(81, 18);
+			this.radioButtonAll.Size = new System.Drawing.Size(71, 15);
 			this.radioButtonAll.TabIndex = 28;
 			this.radioButtonAll.TabStop = true;
 			this.radioButtonAll.Text = "All Phase";
+			this.radioButtonAll.UseSelectable = true;
 			// 
 			// dateTimePickerTo
 			// 
@@ -185,12 +189,11 @@ namespace CAS.UI.UIControls.ForecastControls
 			// labelTo
 			// 
 			this.labelTo.AutoSize = true;
-			this.labelTo.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelTo.ForeColor = System.Drawing.Color.DimGray;
 			this.labelTo.Location = new System.Drawing.Point(24, 36);
 			this.labelTo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.labelTo.Name = "labelTo";
-			this.labelTo.Size = new System.Drawing.Size(30, 17);
+			this.labelTo.Size = new System.Drawing.Size(25, 19);
 			this.labelTo.TabIndex = 26;
 			this.labelTo.Text = "To:";
 			// 
@@ -206,19 +209,18 @@ namespace CAS.UI.UIControls.ForecastControls
 			// labelPeriodFrom
 			// 
 			this.labelPeriodFrom.AutoSize = true;
-			this.labelPeriodFrom.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelPeriodFrom.ForeColor = System.Drawing.Color.DimGray;
 			this.labelPeriodFrom.Location = new System.Drawing.Point(4, 8);
 			this.labelPeriodFrom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.labelPeriodFrom.Name = "labelPeriodFrom";
-			this.labelPeriodFrom.Size = new System.Drawing.Size(50, 17);
+			this.labelPeriodFrom.Size = new System.Drawing.Size(44, 19);
 			this.labelPeriodFrom.TabIndex = 25;
 			this.labelPeriodFrom.Text = "From:";
 			// 
 			// tabControlMain
 			// 
 			this.tabControlMain.Controls.Add(this.tabPageCheck);
-			this.tabControlMain.Location = new System.Drawing.Point(3, 3);
+			this.tabControlMain.Location = new System.Drawing.Point(0, 63);
 			this.tabControlMain.Name = "tabControlMain";
 			this.tabControlMain.SelectedIndex = 0;
 			this.tabControlMain.Size = new System.Drawing.Size(337, 125);
@@ -234,7 +236,7 @@ namespace CAS.UI.UIControls.ForecastControls
             0,
             0,
             131072});
-			this.numericUpDownCycles.Location = new System.Drawing.Point(92, 152);
+			this.numericUpDownCycles.Location = new System.Drawing.Point(89, 212);
 			this.numericUpDownCycles.Maximum = new decimal(new int[] {
             900,
             0,
@@ -254,22 +256,20 @@ namespace CAS.UI.UIControls.ForecastControls
 			// labelCycles
 			// 
 			this.labelCycles.AutoSize = true;
-			this.labelCycles.Font = new System.Drawing.Font("Verdana", 11.25F);
 			this.labelCycles.ForeColor = System.Drawing.Color.DimGray;
-			this.labelCycles.Location = new System.Drawing.Point(11, 156);
+			this.labelCycles.Location = new System.Drawing.Point(8, 212);
 			this.labelCycles.Name = "labelCycles";
-			this.labelCycles.Size = new System.Drawing.Size(77, 18);
+			this.labelCycles.Size = new System.Drawing.Size(64, 19);
 			this.labelCycles.TabIndex = 41;
 			this.labelCycles.Text = "HRS/CYC";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Verdana", 11.25F);
 			this.label1.ForeColor = System.Drawing.Color.DimGray;
-			this.label1.Location = new System.Drawing.Point(11, 181);
+			this.label1.Location = new System.Drawing.Point(8, 237);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(75, 18);
+			this.label1.Size = new System.Drawing.Size(64, 19);
 			this.label1.TabIndex = 50;
 			this.label1.Text = "CYC/DAY";
 			// 
@@ -284,7 +284,7 @@ namespace CAS.UI.UIControls.ForecastControls
             0,
             0,
             131072});
-			this.numericUpDown1.Location = new System.Drawing.Point(92, 177);
+			this.numericUpDown1.Location = new System.Drawing.Point(89, 237);
 			this.numericUpDown1.Maximum = new decimal(new int[] {
             900,
             0,
@@ -306,21 +306,20 @@ namespace CAS.UI.UIControls.ForecastControls
 			this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
 			this.checkBox1.Checked = true;
 			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.checkBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-			this.checkBox1.Location = new System.Drawing.Point(152, 204);
+			this.checkBox1.Location = new System.Drawing.Point(149, 264);
 			this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
 			this.checkBox1.Name = "checkBox1";
-			this.checkBox1.Size = new System.Drawing.Size(165, 18);
+			this.checkBox1.Size = new System.Drawing.Size(139, 15);
 			this.checkBox1.TabIndex = 63;
 			this.checkBox1.Text = "Use current Utilization";
-			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.UseSelectable = true;
 			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
 			// 
 			// ForecastCustomsMTOP
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(337, 277);
+			this.ClientSize = new System.Drawing.Size(337, 337);
 			this.Controls.Add(this.checkBox1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.numericUpDown1);
@@ -335,7 +334,6 @@ namespace CAS.UI.UIControls.ForecastControls
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "ForecastCustomsMTOP";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Forecast Parameters Form";
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
 			this.tabPageCheck.ResumeLayout(false);
@@ -351,20 +349,20 @@ namespace CAS.UI.UIControls.ForecastControls
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label labelHours;
+        private MetroLabel labelHours;
         private System.Windows.Forms.NumericUpDown numericUpDownHours;
 		private System.Windows.Forms.TabPage tabPageCheck;
 		private System.Windows.Forms.DateTimePicker dateTimePickerTo;
-		private System.Windows.Forms.Label labelTo;
+		private MetroLabel labelTo;
 		private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
-		private System.Windows.Forms.Label labelPeriodFrom;
+		private MetroLabel labelPeriodFrom;
 		private System.Windows.Forms.TabControl tabControlMain;
 		private System.Windows.Forms.NumericUpDown numericUpDownCycles;
-		private System.Windows.Forms.Label labelCycles;
-		private System.Windows.Forms.Label label1;
+		private MetroLabel labelCycles;
+		private MetroLabel label1;
 		private System.Windows.Forms.NumericUpDown numericUpDown1;
-		private System.Windows.Forms.RadioButton radioButtonZero;
-		private System.Windows.Forms.RadioButton radioButtonAll;
-		private System.Windows.Forms.CheckBox checkBox1;
+		private MetroRadioButton radioButtonZero;
+		private MetroRadioButton radioButtonAll;
+		private MetroCheckBox checkBox1;
 	}
 }
