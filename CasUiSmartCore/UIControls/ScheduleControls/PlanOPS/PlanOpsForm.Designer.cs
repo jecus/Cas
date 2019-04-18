fb568911c1800b5fcedf0af1733c05eb13abc6bc
Misha <Misha@DESKTOP-2KBGUT9>
2019-04-18 17:06:49 +0600

    PlanOpsForm - Metro

--- CasUiSmartCore/UIControls/ScheduleControls/PlanOPS/PlanOpsForm.Designer.cs
+++ CasUiSmartCore/UIControls/ScheduleControls/PlanOPS/PlanOpsForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace CAS.UI.UIControls.ScheduleControls.PlanOPS
+﻿using MetroFramework.Controls;
+
+namespace CAS.UI.UIControls.ScheduleControls.PlanOPS
 {
 	partial class PlanOpsForm
 	{
@@ -28,27 +30,26 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.Label labelSubType;
-			System.Windows.Forms.Label label1;
-			System.Windows.Forms.Label label11;
+			MetroFramework.Controls.MetroLabel labelSubType;
+			MetroFramework.Controls.MetroLabel label1;
+			MetroFramework.Controls.MetroLabel label11;
 			this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
 			this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
-			this.textBoxRemarks = new System.Windows.Forms.TextBox();
+			this.textBoxRemarks = new MetroFramework.Controls.MetroTextBox();
 			this.buttonCancel = new System.Windows.Forms.Button();
 			this.buttonOk = new System.Windows.Forms.Button();
-			labelSubType = new System.Windows.Forms.Label();
-			label1 = new System.Windows.Forms.Label();
-			label11 = new System.Windows.Forms.Label();
+			labelSubType = new MetroFramework.Controls.MetroLabel();
+			label1 = new MetroFramework.Controls.MetroLabel();
+			label11 = new MetroFramework.Controls.MetroLabel();
 			this.SuspendLayout();
 			// 
 			// labelSubType
 			// 
 			labelSubType.AutoSize = true;
-			labelSubType.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			labelSubType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-			labelSubType.Location = new System.Drawing.Point(12, 15);
+			labelSubType.Location = new System.Drawing.Point(18, 63);
 			labelSubType.Name = "labelSubType";
-			labelSubType.Size = new System.Drawing.Size(43, 14);
+			labelSubType.Size = new System.Drawing.Size(44, 19);
 			labelSubType.TabIndex = 12;
 			labelSubType.Text = "From:";
 			labelSubType.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -56,11 +57,10 @@
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-			label1.Location = new System.Drawing.Point(12, 41);
+			label1.Location = new System.Drawing.Point(18, 89);
 			label1.Name = "label1";
-			label1.Size = new System.Drawing.Size(26, 14);
+			label1.Size = new System.Drawing.Size(25, 19);
 			label1.TabIndex = 15;
 			label1.Text = "To:";
 			label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -68,18 +68,17 @@
 			// label11
 			// 
 			label11.AutoSize = true;
-			label11.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-			label11.Location = new System.Drawing.Point(12, 64);
+			label11.Location = new System.Drawing.Point(18, 115);
 			label11.Name = "label11";
-			label11.Size = new System.Drawing.Size(66, 14);
+			label11.Size = new System.Drawing.Size(62, 19);
 			label11.TabIndex = 276;
 			label11.Text = "Remarks:";
 			label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// dateTimePickerFrom
 			// 
-			this.dateTimePickerFrom.Location = new System.Drawing.Point(84, 12);
+			this.dateTimePickerFrom.Location = new System.Drawing.Point(90, 63);
 			this.dateTimePickerFrom.Name = "dateTimePickerFrom";
 			this.dateTimePickerFrom.Size = new System.Drawing.Size(231, 20);
 			this.dateTimePickerFrom.TabIndex = 14;
@@ -89,20 +88,42 @@
 			// dateTimePickerTo
 			// 
 			this.dateTimePickerTo.Enabled = false;
-			this.dateTimePickerTo.Location = new System.Drawing.Point(84, 38);
+			this.dateTimePickerTo.Location = new System.Drawing.Point(90, 89);
 			this.dateTimePickerTo.Name = "dateTimePickerTo";
 			this.dateTimePickerTo.Size = new System.Drawing.Size(231, 20);
 			this.dateTimePickerTo.TabIndex = 16;
 			// 
 			// textBoxRemarks
 			// 
-			this.textBoxRemarks.Location = new System.Drawing.Point(84, 64);
+			// 
+			// 
+			// 
+			this.textBoxRemarks.CustomButton.Image = null;
+			this.textBoxRemarks.CustomButton.Location = new System.Drawing.Point(168, 2);
+			this.textBoxRemarks.CustomButton.Name = "";
+			this.textBoxRemarks.CustomButton.Size = new System.Drawing.Size(61, 61);
+			this.textBoxRemarks.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+			this.textBoxRemarks.CustomButton.TabIndex = 1;
+			this.textBoxRemarks.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+			this.textBoxRemarks.CustomButton.UseSelectable = true;
+			this.textBoxRemarks.CustomButton.Visible = false;
+			this.textBoxRemarks.Lines = new string[0];
+			this.textBoxRemarks.Location = new System.Drawing.Point(90, 115);
 			this.textBoxRemarks.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
 			this.textBoxRemarks.MaxLength = 1024;
 			this.textBoxRemarks.Multiline = true;
 			this.textBoxRemarks.Name = "textBoxRemarks";
-			this.textBoxRemarks.Size = new System.Drawing.Size(231, 66);
+			this.textBoxRemarks.PasswordChar = '\0';
+			this.textBoxRemarks.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.textBoxRemarks.SelectedText = "";
+			this.textBoxRemarks.SelectionLength = 0;
+			this.textBoxRemarks.SelectionStart = 0;
+			this.textBoxRemarks.ShortcutsEnabled = true;
+			this.textBoxRemarks.Size = new System.Drawing.Size(232, 66);
 			this.textBoxRemarks.TabIndex = 275;
+			this.textBoxRemarks.UseSelectable = true;
+			this.textBoxRemarks.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+			this.textBoxRemarks.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
 			// 
 			// buttonCancel
 			// 
@@ -110,7 +131,7 @@
 			this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonCancel.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.buttonCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonCancel.Location = new System.Drawing.Point(166, 146);
+			this.buttonCancel.Location = new System.Drawing.Point(155, 192);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(75, 33);
 			this.buttonCancel.TabIndex = 278;
@@ -123,7 +144,7 @@
 			this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonOk.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
 			this.buttonOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-			this.buttonOk.Location = new System.Drawing.Point(247, 146);
+			this.buttonOk.Location = new System.Drawing.Point(236, 192);
 			this.buttonOk.Name = "buttonOk";
 			this.buttonOk.Size = new System.Drawing.Size(85, 33);
 			this.buttonOk.TabIndex = 277;
@@ -134,7 +155,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(335, 181);
+			this.ClientSize = new System.Drawing.Size(335, 237);
 			this.Controls.Add(this.buttonCancel);
 			this.Controls.Add(this.buttonOk);
 			this.Controls.Add(label11);
@@ -147,10 +168,11 @@
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "PlanOpsForm";
+			this.Resizable = false;
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "PlanOpsForm";
+			this.Text = "Plan Ops Form";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -160,7 +182,7 @@
 
 		private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
 		private System.Windows.Forms.DateTimePicker dateTimePickerTo;
-		private System.Windows.Forms.TextBox textBoxRemarks;
+		private MetroTextBox textBoxRemarks;
 		private System.Windows.Forms.Button buttonCancel;
 		private System.Windows.Forms.Button buttonOk;
 	}
