b0f8e1c1a8b1f71dae3c2a327b6387aa759a6f39
Misha <Misha@DESKTOP-2KBGUT9>
2019-04-18 22:45:59 +0600

    ForecastFilterAircraft - Metro

--- CasUiSmartCore/UIControls/ForecastControls/ForecastFilterAircraft.Designer.cs
+++ CasUiSmartCore/UIControls/ForecastControls/ForecastFilterAircraft.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace CAS.UI.UIControls.ForecastControls
+﻿using MetroFramework.Controls;
+
+namespace CAS.UI.UIControls.ForecastControls
 {
     partial class ForecastFilterAircraft
     {
@@ -28,144 +30,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.listView2 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(147, 101);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 22);
-            this.button1.TabIndex = 1;
-            this.button1.Text = ">>";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(147, 157);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 22);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Clear>>";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(147, 73);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(57, 22);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "All >>";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 5);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "All Aircraft";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(207, 5);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Filter aircraft";
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new MetroLabel();
+			this.label2 = new MetroLabel();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.listView2 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(143, 156);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(57, 22);
+			this.button1.TabIndex = 1;
+			this.button1.Text = ">>";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(143, 212);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(57, 22);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Clear>>";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(143, 128);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(57, 22);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "All >>";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 60);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "All Aircraft";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(203, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(64, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Filter aircraft";
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2});
-            this.listView1.Location = new System.Drawing.Point(12, 26);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(129, 257);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Name";
-            this.columnHeader2.Width = 100;
-            // 
-            // listView2
-            // 
-            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.listView1.Location = new System.Drawing.Point(8, 81);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(129, 257);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Name";
+			this.columnHeader2.Width = 100;
+			// 
+			// listView2
+			// 
+			this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
-            this.listView2.Location = new System.Drawing.Point(210, 26);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(129, 257);
-            this.listView2.TabIndex = 3;
-            this.listView2.UseCompatibleStateImageBehavior = false;
-            this.listView2.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Name";
-            this.columnHeader1.Width = 100;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(147, 129);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(57, 22);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "<<";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.Button4Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(147, 185);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(57, 22);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Apply";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.Button5Click);
-            // 
-            // ForecastFilterAircraft
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 295);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.listView2);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "ForecastFilterAircraft";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Filter to aircraft for forecast";
-            this.Load += new System.EventHandler(this.ForecastFilterAircraftLoad);
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ForecastFilterAircraftFormClosed);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.listView2.Location = new System.Drawing.Point(206, 81);
+			this.listView2.Name = "listView2";
+			this.listView2.Size = new System.Drawing.Size(129, 257);
+			this.listView2.TabIndex = 3;
+			this.listView2.UseCompatibleStateImageBehavior = false;
+			this.listView2.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Name";
+			this.columnHeader1.Width = 100;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(143, 184);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(57, 22);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "<<";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(143, 240);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(57, 22);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "Apply";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// ForecastFilterAircraft
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(342, 350);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.listView2);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "ForecastFilterAircraft";
+			this.ShowIcon = false;
+			this.Text = "Filter to aircraft for forecast";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ForecastFilterAircraftFormClosed);
+			this.Load += new System.EventHandler(this.ForecastFilterAircraftLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -174,8 +175,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private MetroLabel label1;
+        private MetroLabel label2;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ListView listView2;
