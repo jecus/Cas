9f7e808d50d14b1b2f32d557764f7b862c13d01f
Evgenii Babak <evgenii.babak@gmail.com>
2019-06-14 16:56:51 +0300

    Универсальный грид!!!!!!

---
+++ CasUiSmartCore/UIControls/NewGrid/CommonGridViewControl.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CAS.UI.UIControls.NewGrid
+{
+	partial class CommonGridViewControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+			this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+			this.telerikMetroBlueTheme1 = new Telerik.WinControls.Themes.TelerikMetroBlueTheme();
+			this.panelBottomContainer = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+			this.panelBottomContainer.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// radGridView1
+			// 
+			this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.radGridView1.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+			this.radGridView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
+			this.radGridView1.Location = new System.Drawing.Point(0, 0);
+			// 
+			// 
+			// 
+			this.radGridView1.MasterTemplate.AllowAddNewRow = false;
+			this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition1;
+			this.radGridView1.Name = "radGridView1";
+			this.radGridView1.ReadOnly = true;
+			// 
+			// 
+			// 
+			this.radGridView1.RootElement.EnableRippleAnimation = true;
+			this.radGridView1.Size = new System.Drawing.Size(800, 575);
+			this.radGridView1.TabIndex = 0;
+			this.radGridView1.ThemeName = "TelerikMetroBlue";
+			this.radGridView1.DoubleClick += RadGridView1_DoubleClick;
+			this.radGridView1.KeyDown += RadGridView1_KeyDown;
+			this.radGridView1.ContextMenuOpening += RadGridView1_ContextMenuOpening;
+			// 
+			// panelBottomContainer
+			// 
+			this.panelBottomContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
+			this.panelBottomContainer.Controls.Add(this.label1);
+			this.panelBottomContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelBottomContainer.Location = new System.Drawing.Point(0, 575);
+			this.panelBottomContainer.Name = "panelBottomContainer";
+			this.panelBottomContainer.Size = new System.Drawing.Size(800, 25);
+			this.panelBottomContainer.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.label1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
+			this.label1.Location = new System.Drawing.Point(722, 0);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 25, 0);
+			this.label1.Size = new System.Drawing.Size(78, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Total:";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
+			this.panel1.Controls.Add(this.radGridView1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 575);
+			this.panel1.TabIndex = 5;
+			// 
+			// BaseGridViewControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panelBottomContainer);
+			this.Name = "BaseGridViewControl";
+			this.Size = new System.Drawing.Size(800, 600);
+			((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+			this.panelBottomContainer.ResumeLayout(false);
+			this.panelBottomContainer.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		public Telerik.WinControls.UI.RadGridView radGridView1;
+		private Telerik.WinControls.Themes.TelerikMetroBlueTheme telerikMetroBlueTheme1;
+		private System.Windows.Forms.Panel panelBottomContainer;
+		public System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ CasUiSmartCore/UIControls/NewGrid/CommonGridViewControl.cs
@@ -0,0 +1,658 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CAS.UI.Interfaces;
+using CAS.UI.Management.Dispatchering;
+using CAS.UI.UIControls.Auxiliary;
+using CASTerms;
+using Microsoft.VisualBasic.Devices;
+using SmartCore.Entities.Collections;
+using SmartCore.Entities.Dictionaries;
+using SmartCore.Entities.General;
+using SmartCore.Entities.General.Attributes;
+using SmartCore.Entities.General.Interfaces;
+using Telerik.WinControls.UI;
+
+namespace CAS.UI.UIControls.NewGrid
+{
+	public partial class CommonGridViewControl : UserControl
+	{
+		#region Fields
+		private Type _viewedType;
+		//заголовки списка
+		protected readonly List<GridViewDataColumn> ColumnHeaderList = new List<GridViewDataColumn>();
+		//коллекция выделенных элементов
+		private readonly List<BaseEntityObject> _items = new List<BaseEntityObject>();
+		private RadDropDownMenu _customMenu;
+
+		protected ICommonCollection _selectedItemsList;
+
+		#endregion
+
+		#region Properties
+
+		#region public Type ViewedType
+		/// <summary>
+		/// Тип, объекты которого будут отображаться в списке
+		/// </summary>
+		public Type ViewedType
+		{
+			get { return _viewedType; }
+			set
+			{
+				#region Проверка типа
+				if (value == null)
+					throw new ArgumentNullException("value", "type must be not null");
+				//Проверка, является ли переданный тип наследником BaseSmartCoreObject
+				if (!value.IsSubclassOf(typeof(BaseEntityObject)))
+				{
+					//цикл прошел до низа иерархии и невстретил тип BaseSmartCoreObject
+					//значит переданный тип не является его наследником
+					throw new ArgumentNullException("value", "not inherit from " + typeof(BaseEntityObject).Name);
+				}
+				#endregion
+
+				_viewedType = value;
+
+				try
+				{
+					if (_selectedItemsList != null) _selectedItemsList.Clear();
+					Type genericType = typeof(CommonCollection<>);
+					Type genericList = genericType.MakeGenericType(_viewedType);
+					_selectedItemsList = (ICommonCollection)Activator.CreateInstance(genericList);
+				}
+				catch (Exception ex)
+				{
+					Program.Provider.Logger.Log("Error while building collection", ex);
+					return;
+				}
+
+				try
+				{
+					FirstLoad();
+				}
+				catch (Exception ex)
+				{
+					Program.Provider.Logger.Log("Error while building control", ex);
+					return;
+				}
+			}
+		}
+		#endregion
+
+		#region public Action MenuOpeningAction { get; set; }
+
+		public Action MenuOpeningAction { get; set; }
+
+		#endregion
+
+		#region public RadDropDownMenu CustomMenu
+
+		public RadDropDownMenu CustomMenu
+		{
+			get => _customMenu ?? (_customMenu = new RadDropDownMenu());
+			set => _customMenu = value;
+		}
+
+		#endregion
+
+		#region ItemsCount
+
+		public int ItemsCount => radGridView1.Rows.Count;
+
+		#endregion
+
+		#region public List<T> SelectedItems
+		/// <summary>
+		/// Возвращает выбранные элементы
+		/// </summary>
+		public virtual ICommonCollection SelectedItems
+		{
+			get
+			{
+				if (_selectedItemsList == null)
+					return null;
+
+				_selectedItemsList.Clear();
+				_selectedItemsList.AddRange(radGridView1.SelectedRows
+					.Cast<GridViewDataRowInfo>()
+					.Where(i => i.Tag != null && i.Tag is BaseEntityObject)
+					.Select(i => i.Tag as BaseEntityObject)
+					.ToArray());
+				return _selectedItemsList;
+			}
+		}
+		#endregion
+
+		#region public T SelectedItem
+		/// <summary>
+		/// Выбранный элемент
+		/// </summary>
+		public virtual BaseEntityObject SelectedItem
+		{
+			get
+			{
+				if (radGridView1.CurrentRow != null)
+					return (radGridView1.CurrentRow.Tag as BaseEntityObject);
+				return null;
+			}
+		}
+		#endregion
+
+		#region public IDisplayer Displayer
+		/// <summary>
+		/// Displayer for displaying entity
+		/// </summary>
+		public IDisplayer Displayer { get; set; }
+
+		#endregion
+
+		#region public string DisplayerText { get; set;}
+		/// <summary>
+		/// Text of page's header that Reference lead to
+		/// </summary>
+		public string DisplayerText { get; set; }
+		#endregion
+
+		#region public IDisplayingEntity Entity
+
+		/// <summary>
+		/// Entity to display
+		/// </summary>
+		public IDisplayingEntity Entity { get; set; }
+
+		#endregion
+
+		#region public ReflectionTypes ReflectionType
+
+		/// <summary>
+		/// Type of reflection
+		/// </summary>
+		public ReflectionTypes ReflectionType { get; set; }
+
+		#endregion
+
+		#endregion
+
+		#region Constructor
+
+		public CommonGridViewControl()
+		{
+			InitializeComponent();
+			radGridView1.SelectionMode = GridViewSelectionMode.FullRowSelect;
+			radGridView1.MultiSelect = true;
+		}
+
+
+		#endregion
+
+
+		#region protected virtual void SetHeaders()
+		/// <summary>
+		/// Устанавливает заголовки
+		/// </summary>
+		protected virtual void SetHeaders()
+		{
+			var properties = GetTypeProperties();
+
+			radGridView1.Columns.Clear();
+			GridViewDataColumn columnHeader;
+			foreach (var propertyInfo in properties)
+			{
+				var attr = (ListViewDataAttribute)propertyInfo.GetCustomAttributes(typeof(ListViewDataAttribute), false)[0];
+				columnHeader = new GridViewBrowseColumn(attr.Title);
+				columnHeader.Width = attr.HeaderWidth > 1 ? (int)attr.HeaderWidth : ((int)(radGridView1.Width * attr.HeaderWidth) * 2);
+				columnHeader.Tag = propertyInfo.PropertyType;
+
+				ColumnHeaderList.Add(columnHeader);
+			}
+		}
+		#endregion
+
+		#region public void AddHeader(string title, int? size = null)
+
+		public void AddHeader(string title, int? size = null)
+		{
+			var col = new GridViewBrowseColumn(title);
+
+			if (size.HasValue)
+				col.Width = size.Value;
+			else col.AutoSizeMode = BestFitColumnMode.DisplayedCells;
+
+			ColumnHeaderList.Add(col);
+		}
+
+		#endregion
+
+		#region private List<PropertyInfo> GetTypeProperties()
+		/// <summary>
+		/// Получает свойства типа, на основе которых будут созданы колонки 
+		/// </summary>
+		/// <returns></returns>
+		protected virtual List<PropertyInfo> GetTypeProperties()
+		{
+			if (_viewedType == null)
+				throw new NullReferenceException("_viewedType is null");
+			//определение своиств, имеющих атрибут "отображаемое в списке"
+			var properties =
+				_viewedType.GetProperties().Where(p => p.GetCustomAttributes(typeof(ListViewDataAttribute), false).Length != 0).ToList();
+
+			//поиск своиств у которых задан порядок отображения
+			//своиства, имеющие порядок отображения
+			var orderedProperties = new Dictionary<int, PropertyInfo>();
+			//своиства, НЕ имеющие порядок отображения
+			var unOrderedProperties = new List<PropertyInfo>();
+			foreach (var propertyInfo in properties)
+			{
+				var lvda = (ListViewDataAttribute)
+					propertyInfo.GetCustomAttributes(typeof(ListViewDataAttribute), false).FirstOrDefault();
+				if (lvda.Order > 0) orderedProperties.Add(lvda.Order, propertyInfo);
+				else unOrderedProperties.Add(propertyInfo);
+			}
+
+			var ordered = orderedProperties.OrderBy(p => p.Key).ToList();
+
+			properties.Clear();
+			properties.AddRange(ordered.Select(keyValuePair => keyValuePair.Value));
+			properties.AddRange(unOrderedProperties);
+
+			return properties;
+		}
+		#endregion
+
+		public virtual void SetItemsArray(IEnumerable<BaseEntityObject> itemsArray)
+		{
+			if(itemsArray == null)
+				throw new ArgumentNullException("itemsArray", "itemsArray can't be null");
+			//очищение предварительной коллекции элементов
+			_items.Clear();
+			_items.AddRange(itemsArray);
+			radGridView1.Rows.Clear();
+
+			try
+			{
+				AddItems(itemsArray);
+				SetItemsColor();
+				SetTotalText();
+
+				radGridView1.RowFormatting += RadGridView1_RowFormatting;
+
+			}
+			catch (Exception ex)
+			{
+				Program.Provider.Logger.Log("Error while deleting data", ex);
+				return;
+			}
+		}
+
+		#region public virtual void InsertItems(T[] itemsArray)
+		/// <summary>
+		/// Добавляет элементы в начало ListView
+		/// </summary>
+		/// <param name="itemsArray"></param>
+		public virtual void InsertItems(List<BaseEntityObject> itemsArray)
+		{
+			if (itemsArray.Count == 0)
+				return;
+
+			var temp = new List<GridViewDataRowInfo>();
+			foreach (var item in itemsArray)
+			{
+				var i = 0;
+				var rowInfo = new GridViewDataRowInfo(this.radGridView1.MasterView) { AllowResize = true };
+				rowInfo.Tag = item;
+
+				foreach (var cell in GetListViewSubItems(item))
+				{
+					rowInfo.Cells[i].Value = cell;
+
+					if (cell.ForeColor.HasValue)
+						rowInfo.Cells[i].Style.ForeColor = cell.ForeColor.Value;
+
+					i++;
+				}
+				temp.Add(rowInfo);
+			}
+
+			radGridView1.BeginUpdate();
+			radGridView1.Rows.AddRange(temp.ToArray());
+			radGridView1.EndUpdate();
+
+			SetItemsColor();
+			SetTotalText();
+		}
+
+		#endregion
+
+		#region public virtual T[] GetItemsArray()
+		/// <summary>
+		/// Метод возвращает массив директив
+		/// </summary>
+		/// <returns>Массив директив</returns>
+		public virtual ICommonCollection GetItemsArray()
+		{
+			try
+			{
+				Type genericType = typeof(CommonCollection<>);
+				Type genericList = genericType.MakeGenericType(_viewedType);
+				ICommonCollection returnDetailArray = (ICommonCollection)Activator.CreateInstance(genericList);
+				if (ItemsCount > 0)
+				{
+					for (int i = 0; i < ItemsCount; i++)
+					{
+						returnDetailArray.Add(_items[i]);
+					}
+				}
+				return returnDetailArray;
+			}
+			catch (Exception ex)
+			{
+				Program.Provider.Logger.Log("Error while building collection", ex);
+				return null;
+			}
+		}
+		#endregion
+
+		#region public CustomCell CreateRow(string text, object tag)
+
+		public CustomCell CreateRow(string text, object tag, Color? foreColor = null)
+		{
+			return new CustomCell()
+			{
+				Text = text,
+				Tag = tag,
+				ForeColor = foreColor
+			};
+		}
+
+		#endregion
+
+		#region private void AddItems(T[] itemsArray)
+		/// <summary>
+		/// Добавляет элементы в ListView
+		/// </summary>
+		/// <param name="itemsArray"></param>
+		private void AddItems(IEnumerable<BaseEntityObject> itemsArray)
+		{
+			try
+			{
+				var temp = new List<GridViewDataRowInfo>();
+				foreach (var item in itemsArray)
+				{
+					var i = 0;
+					var rowInfo = new GridViewDataRowInfo(this.radGridView1.MasterView){AllowResize = true};
+					rowInfo.Tag = item;
+
+					foreach (var cell in GetListViewSubItems(item))
+					{
+						rowInfo.Cells[i].Value = cell;
+
+						if(cell.ForeColor.HasValue)
+							rowInfo.Cells[i].Style.ForeColor = cell.ForeColor.Value;
+
+						i++;
+					}
+					temp.Add(rowInfo);
+				}
+
+				radGridView1.BeginUpdate();
+				radGridView1.Rows.AddRange(temp.ToArray());
+				radGridView1.EndUpdate();
+			}
+			catch (Exception ex)
+			{
+				Program.Provider.Logger.Log(ex);
+			}
+		}
+
+		#endregion
+
+		#region protected List<CustomCell> GetListViewSubItems(T item)
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		protected virtual List<CustomCell> GetListViewSubItems(BaseEntityObject item)
+		{
+			var properties = GetTypeProperties();
+
+			var subItems = new List<CustomCell>();
+			foreach (var property in properties)
+			{
+				var value = property.GetValue(item, null);
+				if (value != null)
+				{
+					if (property.Name == "CorrectorId")
+						value = GlobalObjects.CasEnvironment.GetCorrector((int)value);
+
+					string valueString;
+					if (value is DateTime)
+						valueString = SmartCore.Auxiliary.Convert.GetDateFormat((DateTime)value);
+					else if (value is double)
+						valueString = ((double)value).ToString("F");
+					else valueString = value.ToString();
+
+					subItems.Add(new CustomCell()
+					{
+						Text = valueString,
+						Tag = value
+					});
+				}
+				else
+				{
+					subItems.Add(new CustomCell()
+					{
+						Text = "",
+						Tag = ""
+					});
+				}
+			}
+			return subItems;
+		}
+
+		#endregion
+
+		#region private void SetItemsColor()
+		private void SetItemsColor()
+		{
+			foreach (var item in radGridView1.Rows)
+				SetItemColor(item, (BaseEntityObject)item.Tag);
+		}
+		#endregion
+
+		#region protected virtual void SetItemColor(GridViewRowInfo listViewItem, T item)
+		protected virtual void SetItemColor(GridViewRowInfo listViewItem, BaseEntityObject item)
+		{
+			var imd = item as IDirective;
+			if (imd == null) return;
+
+			foreach (GridViewCellInfo cell in listViewItem.Cells)
+			{
+				cell.Style.CustomizeFill = true;
+
+				if (imd.NextPerformanceIsBlocked)
+				{
+					cell.Style.BackColor = Color.FromArgb(Highlight.Grey.Color);
+					//cell.Style.ToolTipText = "This performance is involved on Work Package:" + imd.NextPerformances[0].BlockedByPackage.Title;
+				}
+				else
+				{
+					if (imd.Condition == ConditionState.Overdue)
+						cell.Style.BackColor = Color.FromArgb(Highlight.Red.Color);
+					if (imd.Condition == ConditionState.Notify)
+						cell.Style.BackColor = Color.FromArgb(Highlight.Yellow.Color);
+					if (imd.Condition == ConditionState.NotEstimated)
+						cell.Style.BackColor = Color.FromArgb(Highlight.Blue.Color);
+				}
+			}
+		}
+		#endregion
+
+		#region private void SetTotalText()
+		/// <summary>
+		/// Устанавивает информацию об общем количестве элементов в нижней панели
+		/// </summary>
+		private void SetTotalText()
+		{
+			label1.Text = "Total: " + radGridView1.Rows.Count;
+		}
+
+		#endregion
+
+		#region private void Load()
+
+		private void FirstLoad()
+		{
+			try
+			{
+				SetHeaders();
+				radGridView1.Columns.AddRange(ColumnHeaderList.ToArray());
+			}
+			catch (Exception ex)
+			{
+				Program.Provider.Logger.Log("Error while building control", ex);
+				return;
+			}
+		}
+
+		#endregion
+
+		public void ExportToExcel()
+		{
+			using (var ms = new System.IO.MemoryStream())
+			{
+				var exporter = new Telerik.WinControls.Export.GridViewSpreadExport(radGridView1);
+				var renderer = new Telerik.WinControls.Export.SpreadExportRenderer();
+				 exporter.RunExport(ms, renderer);
+
+				 var sfd = new SaveFileDialog {Filter = ".xlsx Files (*.xlsx)|*.xlsx"};
+
+				 if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					using (var fileStream = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write))
+					{
+						ms.WriteTo(fileStream);
+					}
+					MessageBox.Show("File was success saved!");
+				}
+			}
+		}
+
+		//Events
+
+		#region private void RadGridView1_ContextMenuOpening(object sender, Telerik.WinControls.UI.ContextMenuOpeningEventArgs e)
+
+		private void RadGridView1_ContextMenuOpening(object sender, ContextMenuOpeningEventArgs e)
+		{
+			var cellElement = e.ContextMenuProvider as GridCellElement;
+			if (cellElement == null || cellElement.RowInfo is GridViewFilteringRowInfo || cellElement.RowInfo is GridViewTableHeaderRowInfo)
+				return;
+
+			MenuOpeningAction?.Invoke();
+			e.ContextMenu = _customMenu;
+		}
+
+		#endregion
+
+		#region private void RadGridView1_RowFormatting(object sender, RowFormattingEventArgs e)
+
+		//Колхоз но по другому не знаю как сделать что бы при выделении цвет менял
+		private void RadGridView1_RowFormatting(object sender, RowFormattingEventArgs e)
+		{
+			if (e.RowElement.IsSelected)
+			{
+				foreach (GridViewCellInfo gridViewCellInfo in e.RowElement.RowInfo.Cells)
+					gridViewCellInfo.Style.CustomizeFill = false;
+			}
+			else SetItemColor(e.RowElement.RowInfo, (BaseEntityObject)e.RowElement.RowInfo.Tag);
+		}
+
+		#endregion
+
+		#region private void RadGridView1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+
+		private void RadGridView1_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyData)
+			{
+				case Keys.Enter:
+					if (SelectedItem != null)
+					{
+						if(DisplayerRequested != null)
+							OnDisplayerRequested();
+						else RadGridView1_DoubleClick(sender, e);
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		#endregion
+
+		#region public event EventHandler<ReferenceEventArgs> DisplayerRequested
+		/// <summary>
+		/// Событие, воздуждаемое при необходимости отобразть что-то в новой/текущей вкладке  
+		/// </summary>
+		public event EventHandler<ReferenceEventArgs> DisplayerRequested;
+
+		#endregion
+
+		#region private void RadGridView1_DoubleClick(object sender, System.EventArgs e)
+
+		protected  virtual void RadGridView1_DoubleClick(object sender, EventArgs e)
+		{
+			OnDisplayerRequested();
+		}
+
+		#endregion
+
+		#region protected void OnDisplayerRequested()
+		/// <summary>
+		/// Метод, возбуждающий событие DisplayerRequested
+		/// </summary>
+		protected void OnDisplayerRequested()
+		{
+			if (null != DisplayerRequested)
+			{
+				var reflection = ReflectionType;
+				var k = new Keyboard();
+				if (k.ShiftKeyDown && reflection == ReflectionTypes.DisplayInCurrent)
+					reflection = ReflectionTypes.DisplayInNew;
+				var e = null != Displayer ? new ReferenceEventArgs(Entity, reflection, Displayer, DisplayerText) : new ReferenceEventArgs(Entity, reflection, DisplayerText);
+
+				try
+				{
+					FillDisplayerRequestedParams(e);
+					DisplayerRequested(this, e);
+				}
+				catch (Exception ex)
+				{
+					Program.Provider.Logger.Log("Error while opening record", ex);
+				}
+			}
+		}
+
+		#endregion
+
+		#region protected virtual void FillDisplayerRequestedParams(ReferenceEventArgs e)
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="e"></param>
+		protected virtual void FillDisplayerRequestedParams(ReferenceEventArgs e)
+		{
+			e.DisplayerText = _viewedType.Name;
+			e.RequestedEntity = new CommonListScreen(_viewedType);
+			e.TypeOfReflection = ReflectionTypes.DisplayInNew;
+		}
+		#endregion
+	}
+}
