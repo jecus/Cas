752a290bf7d07134cd97f76c9cb111c2bce87a59
Misha <Misha@DESKTOP-2KBGUT9>
2019-06-18 20:21:06 +0600

    AirFleetWorkPackageList

--- CasUiSmartCore/UIControls/WorkPakage/AirFleetWorkPackageListView.cs
+++ CasUiSmartCore/UIControls/WorkPakage/AirFleetWorkPackageListView.cs
@@ -1,8 +1,6 @@
-﻿using System.Windows.Forms;
-using CAS.UI.Interfaces;
+﻿using CAS.UI.Interfaces;
 using CAS.UI.Management.Dispatchering;
-using CAS.UI.UIControls.Auxiliary;
-using SmartCore.Entities.Dictionaries;
+using CAS.UI.UIControls.NewGrid;
 using SmartCore.Entities.General.WorkPackage;
 
 namespace CAS.UI.UIControls.WorkPakage
@@ -10,7 +8,7 @@ namespace CAS.UI.UIControls.WorkPakage
     ///<summary>
     /// список для отображения ордеров запроса
     ///</summary>
-    public partial class AirFleetWorkPackageListView : BaseListViewControl<WorkPackage>
+    public partial class AirFleetWorkPackageListView : BaseGridViewControl<WorkPackage>
     {
         #region Fields
 
@@ -32,33 +30,33 @@ namespace CAS.UI.UIControls.WorkPakage
 		#region Methods
 
 		#region protected override SetGroupsToItems(int columnIndex)
-		protected override void SetGroupsToItems(int columnIndex)
-        {
-            itemsListView.Groups.Clear();
-            itemsListView.Groups.Add("GroupOpened", "Opened");
-            itemsListView.Groups.Add("GroupPublished", "Published");
-            itemsListView.Groups.Add("GroupClosed", "Closed");
-            itemsListView.Groups.Add("GroupUnknown", "Unknown");
+		//protected override void SetGroupsToItems(int columnIndex)
+  //      {
+  //          itemsListView.Groups.Clear();
+  //          itemsListView.Groups.Add("GroupOpened", "Opened");
+  //          itemsListView.Groups.Add("GroupPublished", "Published");
+  //          itemsListView.Groups.Add("GroupClosed", "Closed");
+  //          itemsListView.Groups.Add("GroupUnknown", "Unknown");
 
-            foreach (ListViewItem item in ListViewItemList)
-            {
-                switch (((WorkPackage)item.Tag).Status)
-                {
-                    case WorkPackageStatus.Closed:
-                        item.Group = itemsListView.Groups[2];
-                        break;
-                    case WorkPackageStatus.Published:
-                        item.Group = itemsListView.Groups[1];
-                        break;
-                    case WorkPackageStatus.Opened:
-                        item.Group = itemsListView.Groups[0];
-                        break;
-                    default:
-                        item.Group = itemsListView.Groups[3];
-                        break;
-                }
-            }
-        }
+  //          foreach (ListViewItem item in ListViewItemList)
+  //          {
+  //              switch (((WorkPackage)item.Tag).Status)
+  //              {
+  //                  case WorkPackageStatus.Closed:
+  //                      item.Group = itemsListView.Groups[2];
+  //                      break;
+  //                  case WorkPackageStatus.Published:
+  //                      item.Group = itemsListView.Groups[1];
+  //                      break;
+  //                  case WorkPackageStatus.Opened:
+  //                      item.Group = itemsListView.Groups[0];
+  //                      break;
+  //                  default:
+  //                      item.Group = itemsListView.Groups[3];
+  //                      break;
+  //              }
+  //          }
+  //      }
         #endregion
 
         #region protected override void FillDisplayerRequestedParams(ReferenceEventArgs e)
