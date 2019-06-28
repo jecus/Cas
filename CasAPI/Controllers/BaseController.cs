6abc6cd1b4b6435ea62a39aa52dc724a7f7732a0
Evgenii Babak <evgenii.babak@gmail.com>
2019-06-28 10:34:24 +0300

    коды ошибок

--- CasAPI/Controllers/BaseController.cs
+++ CasAPI/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EntityCore.DTO;
@@ -24,71 +25,151 @@ namespace CasAPI.Controllers
 		[HttpPost("GetSelectColumnOnly")]
 		public virtual async Task<ActionResult<List<int>>> GetSelectColumnOnly(IEnumerable<Filter> filters, string selectProperty)
 		{
-			var res = await _repository.GetSelectColumnOnlyAsync(filters, selectProperty);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.GetSelectColumnOnlyAsync(filters, selectProperty);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("GetObjectById")]
 		public async Task<ActionResult<T>> GetObjectById(int id, bool loadChild = false)
 		{
-			var res = await _repository.GetObjectByIdAsync(id, loadChild);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.GetObjectByIdAsync(id, loadChild);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("GetObject")]
 		public virtual async Task<ActionResult<T>> GetObject(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false, bool getAll = false)
 		{
-			var res = await _repository.GetObjectAsync(filters, loadChild, getDeleted, getAll);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.GetObjectAsync(filters, loadChild, getDeleted, getAll);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("GetObjectList")]
 		public virtual async Task<ActionResult<List<T>>> GetObjectList(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false)
 		{
-			var res = await _repository.GetObjectListAsync(filters, loadChild, getDeleted);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.GetObjectListAsync(filters, loadChild, getDeleted);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("GetObjectListAll")]
 		public virtual async Task<ActionResult<List<T>>> GetObjectListAll(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false)
 		{
-			var res = await _repository.GetObjectListAllAsync(filters, loadChild, getDeleted);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.GetObjectListAllAsync(filters, loadChild, getDeleted);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("Delete")]
 		public virtual async Task<ActionResult> Delete(T entity)
 		{
-			await _repository.DeleteAsync(entity);
-			return Ok();
+			try
+			{
+				await _repository.DeleteAsync(entity);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("Save")]
 		public virtual async Task<ActionResult<int>> Save(T entity)
 		{
-			var res = await _repository.SaveAsync(entity);
-			return Ok(res);
+			try
+			{
+				var res = await _repository.SaveAsync(entity);
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("BulkInsert")]
 		public virtual async Task<ActionResult> BulkInsert(IEnumerable<T> entity, int? batchSize = null)
 		{
-			await _repository.BulkInsertASync(entity, batchSize);
-			return Ok();
+			try
+			{
+				await _repository.BulkInsertASync(entity, batchSize);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("BulkUpdate")]
 		public virtual async Task<ActionResult> BulkUpdate(IEnumerable<T> entity, int? batchSize = null)
 		{
-			await _repository.BulkUpdateAsync(entity, batchSize);
-			return Ok();
+			try
+			{
+				await _repository.BulkUpdateAsync(entity, batchSize);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 
 		[HttpPost("BulkDelete")]
 		public virtual async Task<ActionResult> BulkDelete(IEnumerable<T> entity, int? batchSize = null)
 		{
-			await  _repository.BulkDeleteAsync(entity, batchSize);
-			return Ok();
+			try
+			{
+				await  _repository.BulkDeleteAsync(entity, batchSize);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
 		}
 	}
 }
