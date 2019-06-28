f1468ed6cb5675026c11433c632199cbd7a3f0d6
Evgenii Babak <evgenii.babak@gmail.com>
2019-06-28 13:31:07 +0300

    monitoring

---
+++ CasAPI/Controllers/MonitoringController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasAPI.Controllers
+{
+	[ApiController]
+	[Route("monitoring")]
+	public class MonitoringController : ControllerBase
+	{
+		public MonitoringController()
+		{
+			
+		}
+
+		[HttpGet("ping")]
+		public ActionResult Ping()
+		{
+			return Ok();
+		}
+	}
+}
