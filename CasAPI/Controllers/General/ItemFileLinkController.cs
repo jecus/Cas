89fe98f0b6c8767f6af80931158eac63205d9fcb
Evgenii Babak <evgenii.babak@gmail.com>
2019-06-30 11:56:12 +0300

    линки забыли

---
+++ CasAPI/Controllers/General/ItemFileLinkController.cs
@@ -0,0 +1,16 @@
+﻿using EntityCore.DTO;
+using EntityCore.DTO.General;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace CasAPI.Controllers.General
+{
+	[Route("itemfilelink")]
+	public class ItemFileLinkController : BaseController<ItemFileLinkDTO>
+	{
+		public ItemFileLinkController(DataContext context, ILogger<BaseController<ItemFileLinkDTO>> logger) : base(context, logger)
+		{
+		}
+	}
+}
