883c2414be6f479c45f4a2b57c6eeb2d4930f62e
Evgenii Babak <evgenii.babak@gmail.com>
2019-07-19 10:15:13 +0300

    fix Average

--- CasUiSmartCore/UIControls/MonthlyUtilizationsControls/AverageUtilizationForm.Designer.cs
+++ CasUiSmartCore/UIControls/MonthlyUtilizationsControls/AverageUtilizationForm.Designer.cs
@@ -81,6 +81,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
 			this.ClientSize = new System.Drawing.Size(302, 217);
 			this.Controls.Add(this.buttonOk);
 			this.Controls.Add(this.buttonCancel);
