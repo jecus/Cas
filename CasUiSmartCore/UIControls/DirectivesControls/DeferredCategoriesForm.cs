cce1f48bb332c7e0578bb6d0f03886b939fdf4be
Misha <Misha@DESKTOP-2KBGUT9>
2019-08-12 20:59:16 +0600

    DeferredCategoriesForm - metro

--- CasUiSmartCore/UIControls/DirectivesControls/DeferredCategoriesForm.cs
+++ CasUiSmartCore/UIControls/DirectivesControls/DeferredCategoriesForm.cs
@@ -5,167 +5,168 @@ using System.Windows.Forms;
 using CASTerms;
 using EntityCore.DTO.Dictionaries;
 using EntityCore.Filter;
+using MetroFramework.Forms;
 using SmartCore.Entities.Dictionaries;
 using SmartCore.Entities.General;
 
 
 namespace CAS.UI.UIControls.DirectivesControls
 {
-    ///<summary>
-    ///Форма отображает категории отложенных дефектов данного самолета
-    ///</summary>
-    public partial class DeferredCategoriesForm : Form
-    {
-        #region Fields
+	///<summary>
+	///Форма отображает категории отложенных дефектов данного самолета
+	///</summary>
+	public partial class DeferredCategoriesForm : MetroForm
+	{
+		#region Fields
 
-        private Aircraft _currentAircraft;
-        private List<DeferredCategory> _defferedCategories;
-        
-        #endregion
+		private Aircraft _currentAircraft;
+		private List<DeferredCategory> _defferedCategories;
+		
+		#endregion
 
-        #region Constructors
+		#region Constructors
 
-        #region public DefferedCategoriesForm()
-        ///<summary>
-        ///Конструктор создающий пустую форму
-        ///</summary>
-        public DeferredCategoriesForm()
-        {
-            InitializeComponent();
-        }
-        #endregion
+		#region public DefferedCategoriesForm()
+		///<summary>
+		///Конструктор создающий пустую форму
+		///</summary>
+		public DeferredCategoriesForm()
+		{
+			InitializeComponent();
+		}
+		#endregion
 
-        #region public DefferedCategoriesForm(Aircraft currentAircraft)
-        ///<summary>
-        ///Конструктор заполняющий всю необходимую информацию о переданном объекте
-        ///</summary>
-        public DeferredCategoriesForm(Aircraft currentAircraft)
-        {
-            InitializeComponent();
+		#region public DefferedCategoriesForm(Aircraft currentAircraft)
+		///<summary>
+		///Конструктор заполняющий всю необходимую информацию о переданном объекте
+		///</summary>
+		public DeferredCategoriesForm(Aircraft currentAircraft)
+		{
+			InitializeComponent();
 
-            _currentAircraft = currentAircraft;
+			_currentAircraft = currentAircraft;
 
-            UpdateInformation();
-        }
+			UpdateInformation();
+		}
 
-        #endregion
+		#endregion
 
-        #endregion
+		#endregion
 
-        #region Properties
-        #endregion
+		#region Properties
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        #region private GetChar(DefferedCategory defferedCategory)
-        ///<summary>
-        /// Возаращает первую букву в названии категории
-        ///</summary>
-        ///<param name="defferedCategory"></param>
-        ///<returns></returns>
-        private char GetChar(DeferredCategory defferedCategory)
-        {
-            return char.ToUpper(defferedCategory.FullName.ToCharArray()[0]);
-        }
-        #endregion
+		#region private GetChar(DefferedCategory defferedCategory)
+		///<summary>
+		/// Возаращает первую букву в названии категории
+		///</summary>
+		///<param name="defferedCategory"></param>
+		///<returns></returns>
+		private char GetChar(DeferredCategory defferedCategory)
+		{
+			return char.ToUpper(defferedCategory.FullName.ToCharArray()[0]);
+		}
+		#endregion
 
-        #region private void UpdateInformation()
-        ///<summary>
-        ///обновление информации в контроле
-        ///</summary>
-        private void UpdateInformation()
-        {
-            if (_currentAircraft == null) return;
+		#region private void UpdateInformation()
+		///<summary>
+		///обновление информации в контроле
+		///</summary>
+		private void UpdateInformation()
+		{
+			if (_currentAircraft == null) return;
 
-            _defferedCategories = new List<DeferredCategory>(GlobalObjects.CasEnvironment.NewLoader.GetObjectListAll<DefferedCategorieDTO, DeferredCategory>
-		        (new Filter("AircraftModelId", _currentAircraft.Model.ItemId),true));
+			_defferedCategories = new List<DeferredCategory>(GlobalObjects.CasEnvironment.NewLoader.GetObjectListAll<DefferedCategorieDTO, DeferredCategory>
+				(new Filter("AircraftModelId", _currentAircraft.Model.ItemId),true));
 
-            listViewDefferedCategories.Items.Clear();
-            List<DeferredCategory> records = new List<DeferredCategory>();
+			listViewDefferedCategories.Items.Clear();
+			List<DeferredCategory> records = new List<DeferredCategory>();
 
-            //LINQ запрос для сортировки записей по дате
-            records.AddRange(_defferedCategories.ToArray());
-            List<DeferredCategory> sortrecords = (from record in records
-                                         orderby GetChar(record) ascending
-                                         select record).ToList();
+			//LINQ запрос для сортировки записей по дате
+			records.AddRange(_defferedCategories.ToArray());
+			List<DeferredCategory> sortrecords = (from record in records
+										 orderby GetChar(record) ascending
+										 select record).ToList();
 
-            foreach (DeferredCategory t in sortrecords)
-            {
-                string[] subs = new[]
-                                    {
-                                        t.FullName,
-                                        t.AircraftModel.ToString(),
-                                        t.Threshold.FirstPerformanceSinceEffectiveDate +
-                                        (!t.Threshold.RepeatInterval.IsNullOrZero() 
-                                             ?  " rpt. "+ t.Threshold.RepeatInterval
-                                             : ""),
-                                    };
+			foreach (DeferredCategory t in sortrecords)
+			{
+				string[] subs = new[]
+									{
+										t.FullName,
+										t.AircraftModel.ToString(),
+										t.Threshold.FirstPerformanceSinceEffectiveDate +
+										(!t.Threshold.RepeatInterval.IsNullOrZero() 
+											 ?  " rpt. "+ t.Threshold.RepeatInterval
+											 : ""),
+									};
 
-                ListViewItem newItem = new ListViewItem(subs) {Tag = t};
-                listViewDefferedCategories.Items.Add(newItem);
-            }
-        }
-        
-        #endregion
+				ListViewItem newItem = new ListViewItem(subs) {Tag = t};
+				listViewDefferedCategories.Items.Add(newItem);
+			}
+		}
+		
+		#endregion
 
-        #region private void ButtonAddClick(object sender, EventArgs e)
+		#region private void ButtonAddClick(object sender, EventArgs e)
 
-        private void ButtonAddClick(object sender, EventArgs e)
-        {
-            DeferredCategoryAddingForm form = new DeferredCategoryAddingForm(_currentAircraft);
-            form.ShowDialog();
+		private void ButtonAddClick(object sender, EventArgs e)
+		{
+			DeferredCategoryAddingForm form = new DeferredCategoryAddingForm(_currentAircraft);
+			form.ShowDialog();
 
-            if(form.DialogResult != DialogResult.OK)return;
+			if(form.DialogResult != DialogResult.OK)return;
 
-            if(form.DefferedCategory != null) _defferedCategories.Add(form.DefferedCategory);
-            UpdateInformation();
-        }
-        #endregion
+			if(form.DefferedCategory != null) _defferedCategories.Add(form.DefferedCategory);
+			UpdateInformation();
+		}
+		#endregion
 
-        #region private void ButtonDeleteClick(object sender, EventArgs e)
+		#region private void ButtonDeleteClick(object sender, EventArgs e)
 
-        private void ButtonDeleteClick(object sender, EventArgs e)
-        {
-            if (listViewDefferedCategories.SelectedItems.Count > 0)
-            {
-                for (int i = 0; i < listViewDefferedCategories.SelectedItems.Count; i++)
-                {
-                    DeferredCategory item = (DeferredCategory)listViewDefferedCategories.SelectedItems[i].Tag;
-                    GlobalObjects.CasEnvironment.Manipulator.Delete(item);
-                }
+		private void ButtonDeleteClick(object sender, EventArgs e)
+		{
+			if (listViewDefferedCategories.SelectedItems.Count > 0)
+			{
+				for (int i = 0; i < listViewDefferedCategories.SelectedItems.Count; i++)
+				{
+					DeferredCategory item = (DeferredCategory)listViewDefferedCategories.SelectedItems[i].Tag;
+					GlobalObjects.CasEnvironment.Manipulator.Delete(item);
+				}
 
-                UpdateInformation();
-            }
-        }
-        #endregion
+				UpdateInformation();
+			}
+		}
+		#endregion
 
-        #region private void ListViewDefferedCategoriesMouseDoubleClick(object sender, MouseEventArgs e)
+		#region private void ListViewDefferedCategoriesMouseDoubleClick(object sender, MouseEventArgs e)
 
-        private void ListViewDefferedCategoriesMouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            if(listViewDefferedCategories.SelectedItems.Count > 0)
-            {
-                DeferredCategoryAddingForm form = new DeferredCategoryAddingForm(_currentAircraft,
-                    (DeferredCategory)listViewDefferedCategories.SelectedItems[0].Tag);
-                form.ShowDialog();
+		private void ListViewDefferedCategoriesMouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			if(listViewDefferedCategories.SelectedItems.Count > 0)
+			{
+				DeferredCategoryAddingForm form = new DeferredCategoryAddingForm(_currentAircraft,
+					(DeferredCategory)listViewDefferedCategories.SelectedItems[0].Tag);
+				form.ShowDialog();
 
-                if(form.DialogResult == DialogResult.OK) UpdateInformation();
-            }
-        }
-        #endregion
+				if(form.DialogResult == DialogResult.OK) UpdateInformation();
+			}
+		}
+		#endregion
 
-        #region private void ListViewDefferedCategoriesMouseClick(object sender, MouseEventArgs e)
+		#region private void ListViewDefferedCategoriesMouseClick(object sender, MouseEventArgs e)
 
-        private void ListViewDefferedCategoriesMouseClick(object sender, MouseEventArgs e)
-        {
-            ButtonDelete.Enabled = listViewDefferedCategories.SelectedItems.Count > 0;
-        }
+		private void ListViewDefferedCategoriesMouseClick(object sender, MouseEventArgs e)
+		{
+			ButtonDelete.Enabled = listViewDefferedCategories.SelectedItems.Count > 0;
+		}
 
-        #endregion
+		#endregion
 
-        #endregion
+		#endregion
 
-        #region Events
-        #endregion
-    }
+		#region Events
+		#endregion
+	}
 }
