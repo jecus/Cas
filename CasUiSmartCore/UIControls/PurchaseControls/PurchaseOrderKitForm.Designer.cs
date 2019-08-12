3802daa55bd60c18adc294be04ac397ab57ad985
Misha <Misha@DESKTOP-2KBGUT9>
2019-08-12 18:57:34 +0600

    PurchaseOrderKitForm - Metro

--- CasUiSmartCore/UIControls/PurchaseControls/PurchaseOrderKitForm.Designer.cs
+++ CasUiSmartCore/UIControls/PurchaseControls/PurchaseOrderKitForm.Designer.cs
@@ -1,33 +1,33 @@
 ﻿namespace CAS.UI.UIControls.PurchaseControls
 {
-    partial class PurchaseOrderKitForm
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class PurchaseOrderKitForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			this.flowLayoutPanelKits = new System.Windows.Forms.FlowLayoutPanel();
 			this.buttonOk = new System.Windows.Forms.Button();
 			this.buttonCancel = new System.Windows.Forms.Button();
@@ -41,19 +41,21 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.flowLayoutPanelKits.AutoScroll = true;
 			this.flowLayoutPanelKits.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.flowLayoutPanelKits.Location = new System.Drawing.Point(0, 0);
+			this.flowLayoutPanelKits.Location = new System.Drawing.Point(1, 63);
 			this.flowLayoutPanelKits.MinimumSize = new System.Drawing.Size(284, 82);
 			this.flowLayoutPanelKits.Name = "flowLayoutPanelKits";
-			this.flowLayoutPanelKits.Size = new System.Drawing.Size(897, 499);
+			this.flowLayoutPanelKits.Size = new System.Drawing.Size(897, 450);
 			this.flowLayoutPanelKits.TabIndex = 0;
 			// 
 			// buttonOk
 			// 
 			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonOk.Location = new System.Drawing.Point(814, 505);
+			this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonOk.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
+			this.buttonOk.Location = new System.Drawing.Point(742, 519);
 			this.buttonOk.Name = "buttonOk";
-			this.buttonOk.Size = new System.Drawing.Size(30, 23);
+			this.buttonOk.Size = new System.Drawing.Size(75, 33);
 			this.buttonOk.TabIndex = 17;
 			this.buttonOk.Text = "Ok";
 			this.buttonOk.UseVisualStyleBackColor = true;
@@ -62,10 +64,12 @@
 			// buttonCancel
 			// 
 			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonCancel.Location = new System.Drawing.Point(850, 505);
+			this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonCancel.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
+			this.buttonCancel.Location = new System.Drawing.Point(823, 519);
 			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(47, 23);
+			this.buttonCancel.Size = new System.Drawing.Size(75, 33);
 			this.buttonCancel.TabIndex = 18;
 			this.buttonCancel.Text = "Cancel";
 			this.buttonCancel.UseVisualStyleBackColor = true;
@@ -76,7 +80,7 @@
 			this.labelCost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.labelCost.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelCost.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-			this.labelCost.Location = new System.Drawing.Point(12, 504);
+			this.labelCost.Location = new System.Drawing.Point(12, 539);
 			this.labelCost.Name = "labelCost";
 			this.labelCost.Size = new System.Drawing.Size(167, 23);
 			this.labelCost.TabIndex = 19;
@@ -88,7 +92,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
-			this.ClientSize = new System.Drawing.Size(901, 529);
+			this.ClientSize = new System.Drawing.Size(901, 564);
 			this.Controls.Add(this.labelCost);
 			this.Controls.Add(this.buttonOk);
 			this.Controls.Add(this.buttonCancel);
@@ -101,13 +105,13 @@
 			this.Text = "Purchase Order Kit Form";
 			this.ResumeLayout(false);
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelKits;
-        public System.Windows.Forms.Button buttonOk;
-        public System.Windows.Forms.Button buttonCancel;
-        private System.Windows.Forms.Label labelCost;
-    }
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelKits;
+		public System.Windows.Forms.Button buttonOk;
+		public System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.Label labelCost;
+	}
 }
