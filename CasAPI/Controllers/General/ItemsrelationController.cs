b09700d59f17b4655a91c21d1512f628118242f0
Evgenii Babak <evgenii.babak@gmail.com>
2019-08-21 13:14:21 +0300

    Проеб в API

---
+++ CasAPI/Controllers/General/ItemsrelationController.cs
@@ -0,0 +1,15 @@
+﻿using EntityCore.DTO;
+using EntityCore.DTO.General;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CasAPI.Controllers.General
+{
+	[Route("itemsrelation")]
+	public class ItemsrelationController : BaseController<ItemsRelationDTO>
+	{
+		public ItemsrelationController(DataContext context, ILogger<BaseController<ItemsRelationDTO>> logger) : base(context, logger)
+		{
+		}
+	}
+}
