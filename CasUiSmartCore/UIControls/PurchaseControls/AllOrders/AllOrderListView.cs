fa949b32c951111c077f1b88a5818cc89c01c54c
Evgenii Babak <evgenii.babak@gmail.com>
2019-09-25 21:21:34 +0300

    fix

--- CasUiSmartCore/UIControls/PurchaseControls/AllOrders/AllOrderListView.cs
+++ CasUiSmartCore/UIControls/PurchaseControls/AllOrders/AllOrderListView.cs
@@ -55,14 +55,14 @@ namespace CAS.UI.UIControls.PurchaseControls.AllOrders
 			else type = "Purchase";
 
 			
-			var status = "1.Opened";
-			if (item.Status == WorkPackageStatus.Published)
-				status = "2.Published";
-			else if (item.Status == WorkPackageStatus.Closed) 
-				status = "3.Closed";
+			//var status = "1.Opened";
+			//if (item.Status == WorkPackageStatus.Published)
+			//	status = "2.Published";
+			//else if (item.Status == WorkPackageStatus.Closed) 
+			//	status = "3.Closed";
 
 			subItems.Add(CreateRow(type, type));
-			subItems.Add(CreateRow(status, item.Status ));
+			subItems.Add(CreateRow(item.Status.ToString(), item.Status ));
 			subItems.Add(CreateRow(item.Number, item.Number ));
 			subItems.Add(CreateRow(item.Title, item.Title ));
 			subItems.Add(CreateRow( "", "" ));
