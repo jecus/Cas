e4f07558cbc280fda9e1f067727e4f1ffb042a69
Misha <Misha@DESKTOP-2KBGUT9>
2020-01-27 15:12:12 +0600

    primeDir - checkbox

--- CasUiSmartCore/UIControls/DirectivesControls/PrimeDirectiveListScreen.Designer.cs
+++ CasUiSmartCore/UIControls/DirectivesControls/PrimeDirectiveListScreen.Designer.cs
@@ -252,6 +252,7 @@ namespace CAS.UI.UIControls.DirectivesControls
 			// 
 			this.checkBoxAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.checkBoxAll.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.checkBoxAll.Visible = false;
 			this.checkBoxAll.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
 			this.checkBoxAll.Location = new System.Drawing.Point(41, 35);
 			this.checkBoxAll.Name = "checkBoxAll";
