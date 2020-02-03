1ff4ddda4f2ac3526fb189c09a1f525794a6e0fa
evgenii.babak@gmail.com <Babak1993>
2020-02-03 17:01:08 +0300

    !

--- CasUiSmartCore/UIControls/MaintananceProgram/CheckNew/MaintenanceCheckFormNew.designer.cs
+++ CasUiSmartCore/UIControls/MaintananceProgram/CheckNew/MaintenanceCheckFormNew.designer.cs
@@ -182,6 +182,7 @@ namespace CAS.UI.UIControls.MaintananceProgram.CheckNew
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(164, 21);
 			this.comboBox1.TabIndex = 28;
+			this.comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
 			// 
 			// buttonApply
 			// 
