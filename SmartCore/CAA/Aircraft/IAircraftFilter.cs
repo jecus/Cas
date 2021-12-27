1d421516494a0961dba6278d41c417b52079ba05
Evgenii Babak <evgenii.babak@gmail.com>
2021-12-27 14:56:56 +0600

    !!!

--- SmartCore/CAA/Aircraft/IAircraftFilter.cs
+++ SmartCore/CAA/Aircraft/IAircraftFilter.cs
@@ -1,10 +1,20 @@
-﻿namespace SmartCore.CAA.Aircraft
+﻿using SmartCore.Entities.General.Attributes;
+
+namespace SmartCore.CAA.Aircraft
 {
     public interface IAircraftFilter
     {
+        [Filter("Registration Number:", Order = 1)]
         string RegistrationNumber { get; set; }
+
+
+        [Filter("Serial Number:", Order = 2)]
         string SerialNumber { get; set; }
+
+        [Filter("Variable Number:", Order = 3)]
         string VariableNumber { get; set; }
+
+        [Filter("Line Number:", Order = 4)]
         string LineNumber { get; set; }
     }
 }
