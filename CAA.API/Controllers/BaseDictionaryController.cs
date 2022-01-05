0211ccd2ffea3bfb9551955d51dfffcb02842208
Evgenii Babak <evgenii.babak@gmail.com>
2022-01-05 18:00:14 +0600

    reFUCK

---
+++ CAA.API/Controllers/BaseDictionaryController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CAA.API.Infrastructure;
+using CAA.Entity.Core;
+using Entity.Abstractions;
+using Entity.Abstractions.Extensions;
+using Entity.Abstractions.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CAA.API.Controllers
+{
+    public class BaseDictionaryController<T> : BaseController<T> where T : BaseEntity,IBaseDictionary
+    {
+		private Type _type = typeof(T);
+
+		public BaseDictionaryController(DataContext context, ILogger<BaseController<T>> logger) : base(context, logger)
+		{
+
+		}
+
+		#region Overrides of BaseController<T>
+
+		public async override Task<ActionResult<T>> GetObjectById(int id, bool loadChild = false)
+		{
+			if (GlobalObjects.Dictionaries.ContainsKey(_type))
+				return GlobalObjects.Dictionaries[_type].Cast<T>().FirstOrDefault(i => i.ItemId == id);
+
+			return await base.GetObjectById(id, loadChild);
+		}
+
+		public async override Task<ActionResult<List<T>>> GetObjectList(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false)
+		{
+			if (GlobalObjects.Dictionaries.ContainsKey(_type) && filters.IsNullOrEmpty())
+				return GlobalObjects.Dictionaries[_type].Cast<T>().ToList();
+
+			return await base.GetObjectList(filters, loadChild, getDeleted);
+		}
+
+		public async override Task<ActionResult<int>> Save(T entity)
+		{
+			try
+			{
+				var res = await _repository.SaveAsync(entity);
+
+				if (GlobalObjects.Dictionaries.ContainsKey(_type))
+				{
+					var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
+					if (find == null)
+						GlobalObjects.Dictionaries[_type].Add(entity);
+					else
+					{
+						GlobalObjects.Dictionaries[_type].Remove(find);
+						GlobalObjects.Dictionaries[_type].Add(entity);
+					}
+
+				}
+
+				return Ok(res);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+				return BadRequest();
+			}
+		}
+
+		public async override Task<ActionResult> Delete(T entity)
+		{
+			if (GlobalObjects.Dictionaries.ContainsKey(_type))
+			{
+				var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
+				if (find != null)
+					GlobalObjects.Dictionaries[_type].Remove(find);
+			}
+
+			return await base.Delete(entity);
+		}
+
+		public async override Task<ActionResult> BulkDelete(IEnumerable<T> entity, int? batchSize = null)
+		{
+			if (GlobalObjects.Dictionaries.ContainsKey(_type))
+			{
+				var ids = entity.Select(i => i.ItemId);
+				var find = GlobalObjects.Dictionaries[_type].Where(i => ids.Contains(i.ItemId)).OfType<T>().ToList();
+				foreach (var dto in find)
+					GlobalObjects.Dictionaries[_type].Remove(dto);
+			}
+
+			return await base.BulkDelete(entity, batchSize);
+		}
+
+
+		#endregion
+	}
+}
--- CAA.API/Controllers/Dictionaries/LicenseRemarkRightController.cs
+++ CAA.API/Controllers/Dictionaries/LicenseRemarkRightController.cs
@@ -1,99 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CAA.API.Infrastructure;
-using CAA.Entity.Core;
+﻿using CAA.Entity.Core;
 using CAA.Entity.Models.Dictionary;
-using Entity.Abstractions.Extensions;
-using Entity.Abstractions.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CAA.API.Controllers.Dictionaries
 {
 	[Route("licenseremarkright")]
-	public class LicenseRemarkRightController : BaseController<CAALicenseRemarkRightDTO>
+	public class LicenseRemarkRightController : BaseDictionaryController<CAALicenseRemarkRightDTO>
 	{
-		private Type _type = typeof(CAALicenseRemarkRightDTO);
 
 		public LicenseRemarkRightController(DataContext context, ILogger<BaseController<CAALicenseRemarkRightDTO>> logger) : base(context, logger)
 		{
 			
 		}
-
-		#region Overrides of BaseController<T>
-
-		public async override Task<ActionResult<CAALicenseRemarkRightDTO>> GetObjectById(int id, bool loadChild = false)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-				return GlobalObjects.Dictionaries[_type].Cast<CAALicenseRemarkRightDTO>().FirstOrDefault(i => i.ItemId == id);
-
-			return await base.GetObjectById(id, loadChild);
-		}
-
-		public async override Task<ActionResult<List<CAALicenseRemarkRightDTO>>> GetObjectList(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type) && filters.IsNullOrEmpty())
-				return GlobalObjects.Dictionaries[_type].Cast<CAALicenseRemarkRightDTO>().ToList();
-
-			return await base.GetObjectList(filters, loadChild, getDeleted);
-		}
-
-		public async override Task<ActionResult<int>> Save(CAALicenseRemarkRightDTO entity)
-		{
-			try
-			{
-				var res = await _repository.SaveAsync(entity);
-
-				if (GlobalObjects.Dictionaries.ContainsKey(_type))
-				{
-					var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
-					if (find == null)
-						GlobalObjects.Dictionaries[_type].Add(entity);
-					else
-					{
-						GlobalObjects.Dictionaries[_type].Remove(find);
-						GlobalObjects.Dictionaries[_type].Add(entity);
-					}
-
-				}
-
-				return Ok(res);
-			}
-			catch (Exception e)
-			{
-				_logger.LogError(e.Message);
-				return BadRequest();
-			}
-		}
-
-		public async override Task<ActionResult> Delete(CAALicenseRemarkRightDTO entity)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-			{
-				var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
-				if (find != null)
-					GlobalObjects.Dictionaries[_type].Remove(find);
-			}
-
-			return await base.Delete(entity);
-		}
-
-		public async override Task<ActionResult> BulkDelete(IEnumerable<CAALicenseRemarkRightDTO> entity, int? batchSize = null)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-			{
-				var ids = entity.Select(i => i.ItemId);
-				var find = GlobalObjects.Dictionaries[_type].Where(i => ids.Contains(i.ItemId)).OfType<CAALicenseRemarkRightDTO>().ToList();
-				foreach (var dto in find)
-					GlobalObjects.Dictionaries[_type].Remove(dto);
-			}
-
-			return await base.BulkDelete(entity, batchSize);
-		}
-
-
-		#endregion
-	}
+    }
 }
