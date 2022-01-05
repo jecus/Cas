00074840f3cae7a4d7d9adbb0d368f1031cd7153
Evgenii Babak <evgenii.babak@gmail.com>
2022-01-05 20:08:17 +0600

    !

--- CasApiNew/Controllers/Dictionaries/TripNameController.cs
+++ CasApiNew/Controllers/Dictionaries/TripNameController.cs
@@ -1,100 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Abstractions.Abstractions.Workers;
-using CAS.API.Infrastructure;
-using CAS.Entity.Core;
+﻿using CAS.Entity.Core;
 using CAS.Entity.Models.DTO.Dictionaries;
-using Entity.Abstractions.Extensions;
-using Entity.Abstractions.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CAS.API.Controllers.Dictionaries
 {
 	[Route("tripname")]
-	public class TripNameController : BaseController<TripNameDTO>
+	public class TripNameController : BaseDictionaryController<TripNameDTO>
 	{
-		private Type _type = typeof(TripNameDTO);
-
-		public TripNameController(DataContext context, ILogger<BaseController<TripNameDTO>> logger) : base(context, logger)
+        public TripNameController(DataContext context, ILogger<BaseController<TripNameDTO>> logger) : base(context, logger)
 		{
 			
 		}
-
-		#region Overrides of BaseController<T>
-
-		public async override Task<ActionResult<TripNameDTO>> GetObjectById(int id, bool loadChild = false)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-				return GlobalObjects.Dictionaries[_type].Cast<TripNameDTO>().FirstOrDefault(i => i.ItemId == id);
-
-			return await base.GetObjectById(id, loadChild);
-		}
-
-		public async override Task<ActionResult<List<TripNameDTO>>> GetObjectList(IEnumerable<Filter> filters = null, bool loadChild = false, bool getDeleted = false)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type) && filters.IsNullOrEmpty())
-				return GlobalObjects.Dictionaries[_type].Cast<TripNameDTO>().ToList();
-
-			return await base.GetObjectList(filters, loadChild, getDeleted);
-		}
-
-		public async override Task<ActionResult<int>> Save(TripNameDTO entity)
-		{
-			try
-			{
-				var res = await _repository.SaveAsync(entity);
-
-				if (GlobalObjects.Dictionaries.ContainsKey(_type))
-				{
-					var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
-					if (find == null)
-						GlobalObjects.Dictionaries[_type].Add(entity);
-					else
-					{
-						GlobalObjects.Dictionaries[_type].Remove(find);
-						GlobalObjects.Dictionaries[_type].Add(entity);
-					}
-
-				}
-
-				return Ok(res);
-			}
-			catch (Exception e)
-			{
-				_logger.LogError(e.Message);
-				return BadRequest();
-			}
-		}
-
-		public async override Task<ActionResult> Delete(TripNameDTO entity)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-			{
-				var find = GlobalObjects.Dictionaries[_type].FirstOrDefault(i => i.ItemId == entity.ItemId);
-				if (find != null)
-					GlobalObjects.Dictionaries[_type].Remove(find);
-			}
-
-			return await base.Delete(entity);
-		}
-
-		public async override Task<ActionResult> BulkDelete(IEnumerable<TripNameDTO> entity, int? batchSize = null)
-		{
-			if (GlobalObjects.Dictionaries.ContainsKey(_type))
-			{
-				var ids = entity.Select(i => i.ItemId);
-				var find = GlobalObjects.Dictionaries[_type].Where(i => ids.Contains(i.ItemId)).OfType<TripNameDTO>().ToList();
-				foreach (var dto in find)
-					GlobalObjects.Dictionaries[_type].Remove(dto);
-			}
-
-			return await base.BulkDelete(entity, batchSize);
-		}
-
-
-		#endregion
-	}
+    }
 }
