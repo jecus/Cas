d15f0026cf18a98f93257898251d99b05f2bce84
Evgenii Babak <evgenii.babak@gmail.com>
2022-01-11 16:19:54 +0600

    fix

---
+++ CAA.API/Controllers/General/RootCauseController.cs
@@ -0,0 +1,15 @@
+﻿using CAA.Entity.Core;
+using CAA.Entity.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CAA.API.Controllers.General
+{
+	[Route("rootcause")]
+	public class RootCauseController : BaseController<RootCauseDTO>
+	{
+		public RootCauseController(DataContext context, ILogger<BaseController<RootCauseDTO>> logger) : base(context, logger)
+		{
+		}
+	}
+}
