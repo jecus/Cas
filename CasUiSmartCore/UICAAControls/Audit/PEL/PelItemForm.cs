1ca762e60ed266d338f05d45030fa4173dc177dd
Evgenii Babak <evgenii.babak@gmail.com>
2022-02-10 22:58:26 +0600

    !

---
+++ CasUiSmartCore/UICAAControls/Audit/PEL/PelItemForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using CAA.Entity.Models.DTO;
+using CAS.UI.UIControls.AnimatedBackgroundWorker;
+using CASTerms;
+using Entity.Abstractions.Filters;
+using MetroFramework.Forms;
+using SmartCore.CAA.Check;
+using SmartCore.CAA.PEL;
+using SmartCore.CAA.RoutineAudits;
+using SmartCore.Entities.Collections;
+using SmartCore.Entities.General.Personnel;
+
+namespace CAS.UI.UICAAControls.Audit.PEL
+{
+    public partial class PelItemForm : MetroForm
+    {
+        private readonly int _operatorId;
+        
+        private CommonCollection<CheckLists> _addedChecks = new CommonCollection<CheckLists>();
+        private CommonCollection<CheckLists> _updateChecks = new CommonCollection<CheckLists>();
+        
+        private AnimatedThreadWorker _animatedThreadWorker = new AnimatedThreadWorker();
+
+        public PelItemForm(int operatorId)
+        {
+            _operatorId = operatorId;
+            InitializeComponent();
+            _animatedThreadWorker.DoWork += AnimatedThreadWorkerDoLoad;
+            _animatedThreadWorker.RunWorkerCompleted += BackgroundWorkerRunWorkerLoadCompleted;
+            _animatedThreadWorker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorkerRunWorkerLoadCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            UpdateInformation();
+        }
+
+        private void AnimatedThreadWorkerDoLoad(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void UpdateInformation()
+        {
+            comboBoxPersonel.Items.Clear();
+            comboBoxPersonel.Items.AddRange(PELRole.Items.ToArray());
+            comboBoxPersonel.SelectedItem = PELRole.Unknown;
+            
+            _fromcheckRevisionListView.SetItemsArray(_addedChecks.ToArray());
+            _tocheckRevisionListView.SetItemsArray(_updateChecks.ToArray());
+        }
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                Program.Provider.Logger.Log("Error while save checkList", ex);
+            }
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (_fromcheckRevisionListView.SelectedItems.Count > 0)
+            {
+                foreach (var item in _fromcheckRevisionListView.SelectedItems.ToArray())
+                {
+                    _updateChecks.Add(item);
+                    _addedChecks.Remove(item);
+                }
+                
+                _fromcheckRevisionListView.SetItemsArray(_addedChecks.ToArray());
+                _tocheckRevisionListView.SetItemsArray(_updateChecks.ToArray());
+            }
+        }
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            if (_tocheckRevisionListView.SelectedItems.Count > 0)
+            {
+                foreach (var item in _tocheckRevisionListView.SelectedItems.ToArray())
+                {
+                    _updateChecks.Remove(item);
+                    _addedChecks.Add(item);
+                }
+
+                _fromcheckRevisionListView.SetItemsArray(_addedChecks.ToArray());
+                _tocheckRevisionListView.SetItemsArray(_updateChecks.ToArray());
+            }
+        }
+        private void CheckListRevisionForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
---
+++ CasUiSmartCore/UICAAControls/Audit/PEL/PelItemListView.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using CAS.UI.UIControls.Auxiliary.Comparers;
+using CAS.UI.UIControls.NewGrid;
+using CASTerms;
+using SmartCore.CAA.Check;
+using SmartCore.CAA.PEL;
+using Telerik.WinControls.Data;
+
+namespace CAS.UI.UICAAControls.Audit.PEL
+{
+	///<summary>
+	/// список для отображения сотрудников
+	///</summary>
+	public partial class PelItemListView : BaseGridViewControl<CheckLists>
+	{
+		#region Fields
+
+		#endregion
+
+		#region Constructors
+
+		#region public PersonnelListView()
+		///<summary>
+		///</summary>
+		public PelItemListView()
+		{
+			InitializeComponent();
+			ColumnIndexes = new List<string>()
+			{
+				"Section №", "Part №","SubPart №","Item №"
+			};
+			SortDirection = SortDirection.Desc;
+			EnableCustomSorting = true;
+
+			this.radGridView1.MasterTemplate.GroupComparer = new GroupComparer();
+		}
+
+        public int OperatorId { get; set; }
+
+		#endregion
+
+		#endregion
+
+		#region Methods
+		
+
+		protected override void GroupingItems()
+		{
+			this.radGridView1.GroupDescriptors.Clear();
+			var descriptor = new GroupDescriptor();
+			foreach (var colName in new List<string>{ "Section №" , "Section Name" , "Part №" , "Part Name", "SubPart №","SubPart Name" })
+				descriptor.GroupNames.Add(colName,  ListSortDirection.Ascending);
+			this.radGridView1.GroupDescriptors.Add(descriptor);
+
+            
+		}
+		
+		#region protected override void SetHeaders()
+		/// <summary>
+		/// Устанавливает заголовки
+		/// </summary>
+		protected override void SetHeaders()
+		{
+			AddColumn("Section №", (int)(radGridView1.Width * 0.20f));
+			AddColumn("Section Name", (int)(radGridView1.Width * 0.24f));
+			AddColumn("Part №", (int)(radGridView1.Width * 0.24f));
+			AddColumn("Part Name", (int)(radGridView1.Width * 0.24f));
+			AddColumn("SubPart №", (int)(radGridView1.Width * 0.24f));
+			AddColumn("SubPart Name", (int)(radGridView1.Width * 0.24f));
+			AddColumn("Item №", (int)(radGridView1.Width * 0.3f));
+			AddColumn("Item Name", (int)(radGridView1.Width * 0.5f));
+			AddColumn("Requirement", (int)(radGridView1.Width * 0.3f));
+			AddColumn("Source", (int)(radGridView1.Width * 0.3f));
+			AddColumn("Personnel", (int)(radGridView1.Width * 0.3f));
+			AddColumn("Signer", (int)(radGridView1.Width * 0.3f));
+		}
+		#endregion
+		
+		
+		protected override List<CustomCell> GetListViewSubItems(CheckLists item)
+		{
+			var author = GlobalObjects.CaaEnvironment?.GetCorrector(item);
+
+			var subItems = new List<CustomCell>()
+			{
+				CreateRow(item.Settings.SectionNumber, item.Settings.SectionNumber),
+				CreateRow(item.Settings.SectionName, item.Settings.SectionName),
+				CreateRow(item.Settings.PartNumber, item.Settings.PartNumber),
+				CreateRow(item.Settings.PartName, item.Settings.PartName),
+				CreateRow(item.Settings.SubPartNumber, item.Settings.SubPartNumber),
+				CreateRow(item.Settings.SubPartName, item.Settings.SubPartName),
+				CreateRow(item.Settings.ItemNumber, item.Settings.ItemNumber),
+				CreateRow(item.Settings.ItemtName, item.Settings.ItemtName),
+				CreateRow(item.Settings.Requirement, item.Settings.Requirement),
+				CreateRow(item.Source, item.Source),
+				CreateRow(item.Level.ToString(), item.Level),
+				CreateRow("",""),
+
+				CreateRow(author, author)
+			};
+
+			return subItems;
+		}
+		
+
+		#endregion
+	}
+}
