0f91243d1a7567fea6e05586b04227f08a6b00b5
Evgenii Babak <evgenii.babak@gmail.com>
2022-02-20 17:53:33 +0600

    Routine Audit

--- CasUiSmartCore/UICAAControls/RoutineAudit/RoutineAuditForm.Designer.cs
+++ CasUiSmartCore/UICAAControls/RoutineAudit/RoutineAuditForm.Designer.cs
@@ -34,10 +34,6 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             System.Windows.Forms.Label label5;
             System.Windows.Forms.Label label3;
             System.Windows.Forms.Label label2;
-            this.ButtonDelete = new AvControls.AvButtonT.AvButtonT();
-            this.ButtonAdd = new AvControls.AvButtonT.AvButtonT();
-            this._tocheckListView = new CAS.UI.UICAAControls.CheckList.CheckListView();
-            this._fromcheckListView = new CAS.UI.UICAAControls.CheckList.CheckListView();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.metroTextBoxDescription = new MetroFramework.Controls.MetroTextBox();
@@ -57,7 +53,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Verdana", 9F);
             label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-            label1.Location = new System.Drawing.Point(1043, 94);
+            label1.Location = new System.Drawing.Point(7, 90);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(98, 14);
             label1.TabIndex = 341;
@@ -69,7 +65,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             label4.AutoSize = true;
             label4.Font = new System.Drawing.Font("Verdana", 9F);
             label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-            label4.Location = new System.Drawing.Point(1043, 152);
+            label4.Location = new System.Drawing.Point(7, 148);
             label4.Name = "label4";
             label4.Size = new System.Drawing.Size(82, 14);
             label4.TabIndex = 345;
@@ -81,7 +77,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             label5.AutoSize = true;
             label5.Font = new System.Drawing.Font("Verdana", 9F);
             label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-            label5.Location = new System.Drawing.Point(1043, 253);
+            label5.Location = new System.Drawing.Point(7, 249);
             label5.Name = "label5";
             label5.Size = new System.Drawing.Size(59, 14);
             label5.TabIndex = 347;
@@ -93,7 +89,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             label3.AutoSize = true;
             label3.Font = new System.Drawing.Font("Verdana", 9F);
             label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-            label3.Location = new System.Drawing.Point(1043, 123);
+            label3.Location = new System.Drawing.Point(7, 119);
             label3.Name = "label3";
             label3.Size = new System.Drawing.Size(53, 14);
             label3.TabIndex = 343;
@@ -105,84 +101,12 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("Verdana", 9F);
             label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(122)))), ((int)(((byte)(122)))));
-            label2.Location = new System.Drawing.Point(1044, 69);
+            label2.Location = new System.Drawing.Point(8, 65);
             label2.Name = "label2";
             label2.Size = new System.Drawing.Size(45, 14);
             label2.TabIndex = 351;
             label2.Text = "Code:";
             label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // ButtonDelete
-            // 
-            this.ButtonDelete.ActiveBackColor = System.Drawing.Color.Transparent;
-            this.ButtonDelete.ActiveBackgroundImage = null;
-            this.ButtonDelete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonDelete.FontMain = new System.Drawing.Font("Verdana", 8F);
-            this.ButtonDelete.FontSecondary = new System.Drawing.Font("Verdana", 8F);
-            this.ButtonDelete.ForeColorMain = System.Drawing.SystemColors.ControlText;
-            this.ButtonDelete.ForeColorSecondary = System.Drawing.SystemColors.ControlText;
-            this.ButtonDelete.Icon = global::CAS.UI.Properties.Resources.DeleteIconSmall;
-            this.ButtonDelete.IconLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ButtonDelete.IconNotEnabled = null;
-            this.ButtonDelete.Location = new System.Drawing.Point(915, 688);
-            this.ButtonDelete.Margin = new System.Windows.Forms.Padding(4);
-            this.ButtonDelete.Name = "ButtonDelete";
-            this.ButtonDelete.NormalBackgroundImage = null;
-            this.ButtonDelete.PaddingMain = new System.Windows.Forms.Padding(0);
-            this.ButtonDelete.PaddingSecondary = new System.Windows.Forms.Padding(0);
-            this.ButtonDelete.ShowToolTip = false;
-            this.ButtonDelete.Size = new System.Drawing.Size(122, 22);
-            this.ButtonDelete.TabIndex = 336;
-            this.ButtonDelete.TextAlignMain = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonDelete.TextAlignSecondary = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonDelete.TextMain = "Delete Selected";
-            this.ButtonDelete.TextSecondary = "";
-            this.ButtonDelete.ToolTipText = "";
-            this.ButtonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
-            // 
-            // ButtonAdd
-            // 
-            this.ButtonAdd.ActiveBackColor = System.Drawing.Color.Transparent;
-            this.ButtonAdd.ActiveBackgroundImage = null;
-            this.ButtonAdd.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonAdd.FontMain = new System.Drawing.Font("Verdana", 8F);
-            this.ButtonAdd.FontSecondary = new System.Drawing.Font("Verdana", 8F);
-            this.ButtonAdd.ForeColorMain = System.Drawing.SystemColors.ControlText;
-            this.ButtonAdd.ForeColorSecondary = System.Drawing.SystemColors.ControlText;
-            this.ButtonAdd.Icon = global::CAS.UI.Properties.Resources.AddIconSmall;
-            this.ButtonAdd.IconLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ButtonAdd.IconNotEnabled = null;
-            this.ButtonAdd.Location = new System.Drawing.Point(921, 351);
-            this.ButtonAdd.Margin = new System.Windows.Forms.Padding(4);
-            this.ButtonAdd.Name = "ButtonAdd";
-            this.ButtonAdd.NormalBackgroundImage = null;
-            this.ButtonAdd.PaddingMain = new System.Windows.Forms.Padding(0);
-            this.ButtonAdd.PaddingSecondary = new System.Windows.Forms.Padding(0);
-            this.ButtonAdd.ShowToolTip = false;
-            this.ButtonAdd.Size = new System.Drawing.Size(116, 33);
-            this.ButtonAdd.TabIndex = 335;
-            this.ButtonAdd.TextAlignMain = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonAdd.TextAlignSecondary = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonAdd.TextMain = "Add Selected";
-            this.ButtonAdd.TextSecondary = "";
-            this.ButtonAdd.ToolTipText = "";
-            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
-            // 
-            // _tocheckListView
-            // 
-            this._tocheckListView.Location = new System.Drawing.Point(8, 391);
-            this._tocheckListView.Name = "_tocheckListView";
-            this._tocheckListView.ReflectionType = CAS.UI.Management.Dispatchering.ReflectionTypes.DisplayInCurrent;
-            this._tocheckListView.Size = new System.Drawing.Size(1029, 290);
-            this._tocheckListView.TabIndex = 334;
-            // 
-            // _fromcheckListView
-            // 
-            this._fromcheckListView.Location = new System.Drawing.Point(8, 63);
-            this._fromcheckListView.Name = "_fromcheckListView";
-            this._fromcheckListView.ReflectionType = CAS.UI.Management.Dispatchering.ReflectionTypes.DisplayInCurrent;
-            this._fromcheckListView.Size = new System.Drawing.Size(1029, 290);
-            this._fromcheckListView.TabIndex = 333;
             // 
             // buttonOk
             // 
@@ -190,7 +114,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonOk.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.buttonOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-            this.buttonOk.Location = new System.Drawing.Point(1331, 688);
+            this.buttonOk.Location = new System.Drawing.Point(303, 359);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 33);
             this.buttonOk.TabIndex = 338;
@@ -203,7 +127,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCancel.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.buttonCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(155)))), ((int)(((byte)(246)))));
-            this.buttonCancel.Location = new System.Drawing.Point(1412, 688);
+            this.buttonCancel.Location = new System.Drawing.Point(384, 359);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 33);
             this.buttonCancel.TabIndex = 337;
@@ -226,7 +150,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.metroTextBoxDescription.CustomButton.Visible = false;
             this.metroTextBoxDescription.ForeColor = System.Drawing.Color.DimGray;
             this.metroTextBoxDescription.Lines = new string[0];
-            this.metroTextBoxDescription.Location = new System.Drawing.Point(1146, 150);
+            this.metroTextBoxDescription.Location = new System.Drawing.Point(110, 146);
             this.metroTextBoxDescription.MaxLength = 32767;
             this.metroTextBoxDescription.Multiline = true;
             this.metroTextBoxDescription.Name = "metroTextBoxDescription";
@@ -258,7 +182,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.metroTextBoxRemark.CustomButton.Visible = false;
             this.metroTextBoxRemark.ForeColor = System.Drawing.Color.DimGray;
             this.metroTextBoxRemark.Lines = new string[0];
-            this.metroTextBoxRemark.Location = new System.Drawing.Point(1146, 251);
+            this.metroTextBoxRemark.Location = new System.Drawing.Point(110, 247);
             this.metroTextBoxRemark.MaxLength = 32767;
             this.metroTextBoxRemark.Multiline = true;
             this.metroTextBoxRemark.Name = "metroTextBoxRemark";
@@ -279,7 +203,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.comboBoxProgramType.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.comboBoxProgramType.ForeColor = System.Drawing.Color.DimGray;
             this.comboBoxProgramType.FormattingEnabled = true;
-            this.comboBoxProgramType.Location = new System.Drawing.Point(1146, 94);
+            this.comboBoxProgramType.Location = new System.Drawing.Point(110, 90);
             this.comboBoxProgramType.Name = "comboBoxProgramType";
             this.comboBoxProgramType.Size = new System.Drawing.Size(347, 22);
             this.comboBoxProgramType.TabIndex = 349;
@@ -289,7 +213,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.comboBoxObject.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.comboBoxObject.ForeColor = System.Drawing.Color.DimGray;
             this.comboBoxObject.FormattingEnabled = true;
-            this.comboBoxObject.Location = new System.Drawing.Point(1146, 119);
+            this.comboBoxObject.Location = new System.Drawing.Point(110, 115);
             this.comboBoxObject.Name = "comboBoxObject";
             this.comboBoxObject.Size = new System.Drawing.Size(347, 22);
             this.comboBoxObject.TabIndex = 350;
@@ -310,7 +234,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.metroTextBoxTitle.CustomButton.Visible = false;
             this.metroTextBoxTitle.ForeColor = System.Drawing.Color.DimGray;
             this.metroTextBoxTitle.Lines = new string[0];
-            this.metroTextBoxTitle.Location = new System.Drawing.Point(1147, 67);
+            this.metroTextBoxTitle.Location = new System.Drawing.Point(111, 63);
             this.metroTextBoxTitle.MaxLength = 32767;
             this.metroTextBoxTitle.Multiline = true;
             this.metroTextBoxTitle.Name = "metroTextBoxTitle";
@@ -330,7 +254,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1501, 730);
+            this.ClientSize = new System.Drawing.Size(473, 401);
             this.Controls.Add(this.metroTextBoxTitle);
             this.Controls.Add(label2);
             this.Controls.Add(this.comboBoxObject);
@@ -343,26 +267,17 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             this.Controls.Add(label1);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.ButtonDelete);
-            this.Controls.Add(this.ButtonAdd);
-            this.Controls.Add(this._tocheckListView);
-            this.Controls.Add(this._fromcheckListView);
-            this.Name = "RoutineAuditForm";
             this.MaximizeBox = false;
+            this.Name = "RoutineAuditForm";
             this.Resizable = false;
-            this.Text = "RoutineAuditForm";
+            this.Text = "Add RoutineAuditForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RoutineAuditForm_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
-        private AvControls.AvButtonT.AvButtonT ButtonDelete;
-        private AvControls.AvButtonT.AvButtonT ButtonAdd;
-        private CheckList.CheckListView _tocheckListView;
-        private CheckList.CheckListView _fromcheckListView;
         private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonCancel;
         private MetroFramework.Controls.MetroTextBox metroTextBoxDescription;
--- CasUiSmartCore/UICAAControls/RoutineAudit/RoutineAuditForm.cs
+++ CasUiSmartCore/UICAAControls/RoutineAudit/RoutineAuditForm.cs
@@ -1,30 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
-using CAA.Entity.Models.Dictionary;
-using CAA.Entity.Models.DTO;
-using CAS.UI.UIControls.AnimatedBackgroundWorker;
 using CASTerms;
-using Entity.Abstractions.Filters;
 using MetroFramework.Forms;
 using SmartCore.CAA.Check;
-using SmartCore.CAA.FindingLevel;
 using SmartCore.CAA.RoutineAudits;
-using SmartCore.Calculations;
-using SmartCore.Entities.Dictionaries;
 
 namespace CAS.UI.UICAAControls.RoutineAudit
 {
     public partial class RoutineAuditForm : MetroForm
     {
         private SmartCore.CAA.RoutineAudits.RoutineAudit _audit;
-        private List<CheckLists> _addedChecks = new List<CheckLists>();
-        private List<CheckLists> _updateChecks = new List<CheckLists>();
-        private AnimatedThreadWorker _animatedThreadWorker = new AnimatedThreadWorker();
-        private IList<FindingLevels> _levels = new List<FindingLevels>();
-        private List<RoutineAuditRecord> _records = new List<RoutineAuditRecord>();
 
         public RoutineAuditForm()
         {
@@ -34,63 +20,9 @@ namespace CAS.UI.UICAAControls.RoutineAudit
         public RoutineAuditForm(SmartCore.CAA.RoutineAudits.RoutineAudit audit) : this()
         {
             _audit = audit;
-            _animatedThreadWorker.DoWork += AnimatedThreadWorkerDoLoad;
-            _animatedThreadWorker.RunWorkerCompleted += BackgroundWorkerRunWorkerLoadCompleted;
-            _animatedThreadWorker.RunWorkerAsync();
-        }
-
-        private void BackgroundWorkerRunWorkerLoadCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
             UpdateInformation();
         }
-
-        private void AnimatedThreadWorkerDoLoad(object sender, DoWorkEventArgs e)
-        {
-            _updateChecks.Clear();
-            _addedChecks.Clear();
-            
-            var editions = GlobalObjects.CaaEnvironment.NewLoader.GetObjectList<CheckListRevisionDTO, CheckListRevision>(new List<Filter>()
-            {
-                new Filter("Status", (byte)EditionRevisionStatus.Open),
-                new Filter("Type", (byte)RevisionType.Edition),
-            });
-            if (editions.Any())
-            {
-                var edition = editions.FirstOrDefault();
-                _addedChecks.AddRange(GlobalObjects.CaaEnvironment.NewLoader.GetObjectListAll<CheckListDTO, CheckLists>(new Filter("EditionId", edition.ItemId), loadChild:true));
-		            
-                foreach (var check in _addedChecks)
-                    check.EditionNumber = edition.Number;
-            }
-            _levels.Clear();
-            _levels = GlobalObjects.CaaEnvironment.NewLoader.GetObjectList<FindingLevelsDTO, FindingLevels>(new Filter("OperatorId", _audit.OperatorId));
-
-            foreach (var check in _addedChecks)
-            {
-                check.Level = _levels.FirstOrDefault(i => i.ItemId == check.Settings.LevelId) ??
-                              FindingLevels.Unknown;
-
-
-                check.Remains = Lifelength.Null;
-                check.Condition = ConditionState.Satisfactory;
-            }
-
-
-            if (_audit.ItemId > 0)
-            {
-                _records = GlobalObjects.CaaEnvironment.NewLoader
-                    .GetObjectListAll<RoutineAuditRecordDTO, RoutineAuditRecord>(new Filter("RoutineAuditId", _audit.ItemId), loadChild: true).ToList();
-
-                var ids = _records.Select(i => i.CheckListId);
-                _updateChecks.AddRange(_addedChecks.Where(i => ids.Contains(i.ItemId)));
-
-
-                foreach (var check in _updateChecks)
-                    _addedChecks.Remove(check);
-            }
-
-        }
-
+        
         private void UpdateInformation()
         {
             comboBoxProgramType.Items.Clear();
@@ -104,9 +36,6 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             metroTextBoxTitle.Text = _audit.Title;
             metroTextBoxDescription.Text = _audit.Description;
             metroTextBoxRemark.Text = _audit.Remark;
-
-            _fromcheckListView.SetItemsArray(_addedChecks.ToArray());
-            _tocheckListView.SetItemsArray(_updateChecks.ToArray());
         }
 
 
@@ -145,61 +74,7 @@ namespace CAS.UI.UICAAControls.RoutineAudit
             DialogResult = DialogResult.Cancel;
             Close();
         }
-
-        private void ButtonAdd_Click(object sender, EventArgs e)
-        {
-            if (_fromcheckListView.SelectedItems.Count == 0) return;
-
-            if (string.IsNullOrEmpty(_audit.Title))
-                metroTextBoxTitle.Text = $"{GlobalObjects.CaaEnvironment.IdentityUser.Name} ({SmartCore.Auxiliary.Convert.GetDateFormat(_audit.Settings.Created)} {_audit.Settings.Created.TimeOfDay.Hours}:{_audit.Settings.Created.TimeOfDay.Minutes}:{_audit.Settings.Created.TimeOfDay.Seconds})";
-
-            if(_audit.ItemId <= 0)
-                Save();
-
-            foreach (var item in _fromcheckListView.SelectedItems.ToArray())
-            {
-                _updateChecks.Add(item);
-                _addedChecks.Remove(item);
-
-                var rec = new RoutineAuditRecord()
-                {
-                    CheckListId = item.ItemId,
-                    RoutineAuditId = _audit.ItemId
-                };
-
-                GlobalObjects.CaaEnvironment.NewKeeper.Save(rec);
-                _records.Add(rec);
-            }
-
-            _fromcheckListView.SetItemsArray(_addedChecks.ToArray());
-            _tocheckListView.SetItemsArray(_updateChecks.ToArray());
-        }
-
-        private void ButtonDelete_Click(object sender, EventArgs e)
-        {
-            if (_tocheckListView.SelectedItems.Count == 0) return;
-
-
-            var dialog = MessageBox.Show("Do you really wand delete records?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
-
-            if (dialog == DialogResult.Yes)
-            {
-                foreach (var item in _tocheckListView.SelectedItems.ToArray())
-                {
-                    _updateChecks.Remove(item);
-                    _addedChecks.Add(item);
-
-                    var delete = _records.FirstOrDefault(i => i.CheckListId == item.ItemId);
-                    if(delete != null)
-                        GlobalObjects.CaaEnvironment.NewKeeper.Delete(delete);
-                }
-
-                _fromcheckListView.SetItemsArray(_addedChecks.ToArray());
-                _tocheckListView.SetItemsArray(_updateChecks.ToArray());
-            }
-            
-        }
-
+        
         private void RoutineAuditForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             DialogResult = DialogResult.OK;
