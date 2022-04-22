3b1e5b0b0c1aac1f8c6bbe87d47e30fa44784c49
Evgenii Babak <evgenii.babak@gmail.com>
2022-04-22 14:30:16 +0600

    !

--- CasUiSmartCore/UICAAControls/CAAEducation/EducationScreen.cs
+++ CasUiSmartCore/UICAAControls/CAAEducation/EducationScreen.cs
@@ -24,6 +24,7 @@ namespace CAS.UI.UICAAControls.CAAEducation
             
             aircraftHeaderControl1.Operator = currentOperator;
             statusControl.ShowStatus = true;
+            statusControl.Operator = currentOperator;
             
         }
 
