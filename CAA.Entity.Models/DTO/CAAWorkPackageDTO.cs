2665e51bb52785ed711ca107467eed6f8be52d5b
Evgenii Babak <evgenii.babak@gmail.com>
2022-04-25 15:24:15 +0600

    api wp

---
+++ CAA.Entity.Models/DTO/CAAWorkPackageDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Entity.Abstractions;
+using Entity.Abstractions.Attributte;
+
+namespace CAA.Entity.Models.DTO
+{
+    
+    [Table("WorkPackage", Schema = "dbo")]
+    [Condition("IsDeleted", 0)]
+    public class CAAWorkPackageDTO : BaseEntity, IOperatable
+    {
+        [Column("Title")]
+        public string Title { get; set; }
+        
+        [Column("SettingsJSON")]
+        public string SettingsJSON { get; set; }
+
+        [Column("OperatorId")]
+        public int OperatorId { get; set; }
+        
+    }
+
+}
