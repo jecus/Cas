365a2ec354681fcae46b2c7ce0267b4eb6a29e08
Evgenii Babak <evgenii.babak@gmail.com>
2022-04-26 12:38:21 +0600

    !!!

---
+++ CasUiSmartCore/UICAAControls/WorkPackage/CAAWPRecordListScreen.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using CAA.Entity.Models.DTO;
+using CAS.UI.Interfaces;
+using CAS.UI.UIControls.Auxiliary;
+using CAS.UI.UIControls.FiltersControls;
+using CASTerms;
+using Entity.Abstractions;
+using Entity.Abstractions.Filters;
+using SmartCore.CAA.CAAWP;
+using SmartCore.Entities.Collections;
+using SmartCore.Entities.General;
+using SmartCore.Filters;
+using Telerik.WinControls.UI;
+
+namespace CAS.UI.UICAAControls.WorkPackage
+{
+	///<summary>
+	///</summary>
+	[ToolboxItem(false)]
+	public partial class CAAWPRecordListScreen : ScreenControl
+	{
+		private readonly int _wpId;
+
+		#region Fields
+
+		private CommonCollection<CAAWorkPackageRecord> _initialDocumentArray = new CommonCollection<CAAWorkPackageRecord>();
+		private CommonCollection<CAAWorkPackageRecord> _resultDocumentArray = new CommonCollection<CAAWorkPackageRecord>();
+		private CommonFilterCollection _filter;
+
+		private CAAWPRecordListView _directivesViewer;
+		
+		#endregion
+
+
+		#region Constructors
+
+		#region public RoutineAuditListScreen()
+		///<summary>
+		/// Конструктор по умолчанию
+		///</summary>
+		public CAAWPRecordListScreen()
+		{
+			InitializeComponent();
+		}
+		#endregion
+
+		#region public RoutineAuditListScreen(Operator currentOperator)
+
+		///<summary>
+		/// Создаёт экземпляр элемента управления, отображающего список директив
+		///</summary>
+		///<param name="currentOperator">ВС, которому принадлежат директивы</param>>
+		public CAAWPRecordListScreen(Operator currentOperator, int wpId)
+			: this()
+		{
+			if (currentOperator == null)
+				throw new ArgumentNullException("currentOperator");
+			_wpId = wpId;
+			aircraftHeaderControl1.Operator = currentOperator;
+            statusControl.ShowStatus = false;
+			labelTitle.Visible = false;
+
+			_filter = new CommonFilterCollection(typeof(CAAWorkPackage));
+			
+			InitListView();
+			UpdateInformation();
+
+			if (GlobalObjects.CaaEnvironment.IdentityUser.CAAUserType == CAAUserType.CAA ||
+			    GlobalObjects.CaaEnvironment.IdentityUser.CAAUserType == CAAUserType.Operator)
+			{
+				pictureBox1.Visible = buttonAddNew.Visible  = buttonDeleteSelected.Visible = false;
+			}
+		}
+
+		#endregion
+
+		#endregion
+
+		#region Methods
+
+		#region protected override void AnimatedThreadWorkerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		protected override void AnimatedThreadWorkerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			_directivesViewer.SetItemsArray(_resultDocumentArray.ToArray());
+			headerControl.PrintButtonEnabled = _directivesViewer.ItemsCount != 0;
+			_directivesViewer.Focus();
+		}
+		#endregion
+
+		#region protected override void AnimatedThreadWorkerDoWork(object sender, DoWorkEventArgs e)
+		protected override void AnimatedThreadWorkerDoWork(object sender, DoWorkEventArgs e)
+		{
+			_initialDocumentArray.Clear();
+			_resultDocumentArray.Clear();
+
+			AnimatedThreadWorker.ReportProgress(0, "load directives");
+
+
+			_initialDocumentArray.AddRange(GlobalObjects.CaaEnvironment.NewLoader
+				.GetObjectListAll<CAAWorkPackageRecordDTO, CAAWorkPackageRecord>(new Filter("WorkPackageId", _wpId)));
+			
+			AnimatedThreadWorker.ReportProgress(70, "filter directives");
+
+			FilterItems(_initialDocumentArray, _resultDocumentArray);
+
+			AnimatedThreadWorker.ReportProgress(100, "Complete");
+		}
+		#endregion
+
+		
+		
+
+		#region private void ButtonDeleteClick(object sender, EventArgs e)
+		private void ButtonDeleteClick(object sender, EventArgs e)
+		{
+            _directivesViewer.ButtonDeleteClick(sender, e);
+		}
+
+		#endregion
+
+		#region private void InitListView()
+
+		private void InitListView()
+		{
+			_directivesViewer = new CAAWPRecordListView(AnimatedThreadWorker);
+			_directivesViewer.TabIndex = 2;
+			_directivesViewer.Location = new Point(panel1.Left, panel1.Top);
+			_directivesViewer.Dock = DockStyle.Fill;
+			_directivesViewer.SelectedItemsChanged += DirectivesViewerSelectedItemsChanged;
+			Controls.Add(_directivesViewer);
+			//события 
+			_directivesViewer.SelectedItemsChanged += DirectivesViewerSelectedItemsChanged;
+
+			_directivesViewer.AddMenuItems();
+
+			_directivesViewer.MenuOpeningAction = () =>
+			{
+				if (_directivesViewer.SelectedItems.Count <= 0)
+					return;
+				if (_directivesViewer.SelectedItems.Count == 1)
+				{
+					
+				}
+			};
+			
+			_directivesViewer.DisableDeleteContext();
+			_directivesViewer.DisableCopyPaste();
+
+
+			panel1.Controls.Add(_directivesViewer);
+		}
+
+		#endregion
+		
+
+		
+
+		#region private void DirectivesViewerSelectedItemsChanged(object sender, SelectedItemsChangeEventArgs e)
+
+		private void DirectivesViewerSelectedItemsChanged(object sender, SelectedItemsChangeEventArgs e)
+		{
+			headerControl.EditButtonEnabled = _directivesViewer.SelectedItems.Count > 0;
+		}
+
+		#endregion
+
+		#region private void UpdateInformation()
+		/// <summary>
+		/// Происзодит обновление отображения элементов
+		/// </summary>
+		private void UpdateInformation()
+		{
+			AnimatedThreadWorker.RunWorkerAsync();
+		}
+		#endregion
+
+		#region private void HeaderControlButtonReloadClick(object sender, EventArgs e)
+
+		private void HeaderControlButtonReloadClick(object sender, EventArgs e)
+		{
+			AnimatedThreadWorker.DoWork -= AnimatedThreadWorkerDoWork;
+			AnimatedThreadWorker.DoWork -= AnimatedThreadWorkerDoFilteringWork;
+			AnimatedThreadWorker.DoWork += AnimatedThreadWorkerDoWork;
+			AnimatedThreadWorker.RunWorkerAsync();
+		}
+		#endregion
+
+		#region private void ButtonAddDisplayerRequested(object sender, ReferenceEventArgs e)
+
+		private void ButtonAddDisplayerRequested(object sender, ReferenceEventArgs e)
+		{
+			// var form = new CAAWorkPackageEditorForm(new CAAWorkPackage(){OperatorId = _operatorId});
+			// if(form.ShowDialog() == DialogResult.OK)
+			// 	AnimatedThreadWorker.RunWorkerAsync();
+		}
+
+		#endregion
+
+		#region private void ButtonApplyFilterClick(object sender, EventArgs e)
+
+		private void ButtonApplyFilterClick(object sender, EventArgs e)
+		{
+			var form = new CommonFilterForm(_filter, _initialDocumentArray);
+
+			if (form.ShowDialog(this) == DialogResult.OK)
+			{
+				AnimatedThreadWorker.DoWork -= AnimatedThreadWorkerDoWork;
+				AnimatedThreadWorker.DoWork -= AnimatedThreadWorkerDoFilteringWork;
+				AnimatedThreadWorker.DoWork += AnimatedThreadWorkerDoFilteringWork;
+
+				AnimatedThreadWorker.RunWorkerAsync();
+			}
+		}
+
+		#endregion
+
+		#region private void FilterItems(IEnumerable<Specialist> initialCollection, ICommonCollection<Specialist> resultCollection)
+
+		///<summary>
+		///</summary>
+		///<param name="initialCollection"></param>
+		///<param name="resultCollection"></param>
+		private void FilterItems(IEnumerable<CAAWorkPackageRecord> initialCollection, ICommonCollection<CAAWorkPackageRecord> resultCollection)
+		{
+			if (_filter == null || _filter.All(i => i.Values.Length == 0))
+			{
+				resultCollection.Clear();
+				resultCollection.AddRange(initialCollection);
+				return;
+			}
+
+			resultCollection.Clear();
+
+			foreach (var pd in initialCollection)
+			{
+				if (_filter.FilterTypeAnd)
+				{
+					bool acceptable = true;
+					foreach (ICommonFilter filter in _filter)
+					{
+						acceptable = filter.Acceptable(pd); if (!acceptable) break;
+					}
+					if (acceptable) resultCollection.Add(pd);
+				}
+				else
+				{
+					bool acceptable = true;
+					foreach (ICommonFilter filter in _filter)
+					{
+						if (filter.Values == null || filter.Values.Length == 0)
+							continue;
+						acceptable = filter.Acceptable(pd); if (acceptable) break;
+					}
+					if (acceptable) resultCollection.Add(pd);
+				}
+			}
+		}
+		#endregion
+
+		#region private void AnimatedThreadWorkerDoFilteringWork(object sender, DoWorkEventArgs e)
+
+		private void AnimatedThreadWorkerDoFilteringWork(object sender, DoWorkEventArgs e)
+		{
+			_resultDocumentArray.Clear();
+
+			#region Фильтрация директив
+			AnimatedThreadWorker.ReportProgress(50, "filter directives");
+
+			FilterItems(_initialDocumentArray, _resultDocumentArray);
+
+			if (AnimatedThreadWorker.CancellationPending)
+			{
+				e.Cancel = true;
+				return;
+			}
+			#endregion
+
+			AnimatedThreadWorker.ReportProgress(100, "Complete");
+		}
+
+		#endregion
+
+		#endregion
+
+    }
+}
---
+++ CasUiSmartCore/UICAAControls/WorkPackage/CAAWPRecordListView.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using CAS.UI.Interfaces;
+using CAS.UI.UIControls.AnimatedBackgroundWorker;
+using CAS.UI.UIControls.NewGrid;
+using CASTerms;
+using SmartCore.CAA;
+using SmartCore.CAA.CAAWP;
+using SmartCore.Entities.General;
+
+namespace CAS.UI.UICAAControls.WorkPackage
+{
+	///<summary>
+	/// список для отображения сотрудников
+	///</summary>
+	public partial class CAAWPRecordListView : BaseGridViewControl<CAAWorkPackageRecord>
+	{
+        private readonly AnimatedThreadWorker _animatedThreadWorker;
+        
+        public CAAWPRecordListView()
+        {
+            InitializeComponent();
+		}
+
+        /// <summary>
+        /// </summary>
+        /// <param name="animatedThreadWorker"></param>
+        public CAAWPRecordListView(AnimatedThreadWorker animatedThreadWorker) : this()
+		{
+            _animatedThreadWorker = animatedThreadWorker;
+            SortDirection = SortDirection.Asc;
+			OldColumnIndex = 1;
+		}
+
+        public int OperatorId { get; set; }
+        
+
+		#region Methods
+
+		protected override void GroupingItems()
+		{
+			
+		}
+
+
+		#region protected override void SetHeaders()
+		/// <summary>
+		/// Устанавливает заголовки
+		/// </summary>
+		protected override void SetHeaders()
+		{
+			AddColumn("Signer", (int)(radGridView1.Width * 0.3f));
+		}
+		#endregion
+
+        public override void ButtonDeleteClick(object sender, EventArgs e)
+        {
+            if (this.SelectedItems == null ||
+                this.SelectedItems.Count == 0) return;
+
+            string typeName = nameof(SmartCore.CAA.FindingLevel.FindingLevels);
+
+            DialogResult confirmResult =
+                MessageBox.Show(this.SelectedItems.Count == 1
+                        ? "Do you really want to delete " + typeName + " " + this.SelectedItems[0] + "?"
+                        : "Do you really want to delete selected " + typeName + "s?", "Confirm delete operation",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (confirmResult == DialogResult.Yes)
+            {
+                this.radGridView1.BeginUpdate();
+                GlobalObjects.NewKeeper.Delete(this.SelectedItems.OfType<BaseEntityObject>().ToList(), true);
+                this.radGridView1.EndUpdate();
+                _animatedThreadWorker.RunWorkerAsync();
+            }
+		}
+
+		#region protected override List<CustomCell> GetListViewSubItems(FindingLevels item)
+
+		protected override List<CustomCell> GetListViewSubItems(CAAWorkPackageRecord item)
+        {
+	        var corrector = GlobalObjects.CaaEnvironment?.GetCorrector(item);
+	        
+	        var subItems = new List<CustomCell>()
+			{
+				CreateRow(corrector, corrector)
+			};
+
+			return subItems;
+		}
+
+		#endregion
+		
+		
+		protected override void FillDisplayerRequestedParams(ReferenceEventArgs e)
+		{
+			e.Cancel = true;
+		}
+
+
+		#endregion
+	}
+}
