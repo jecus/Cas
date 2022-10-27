834bea5f20226d10cc039d6a7482a3e6d7693ad1
Evgenii Babak <evgenii.babak@gmail.com>
2022-10-27 14:39:24 +0600

    add ds

---
+++ CASReports/Datasets/CAA/SpecialistDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace CASReports.Datasets.CAA
+{
+
+
+    public partial class ATLBDataSet
+    {
+        partial class DataTable1DataTable
+        {
+        }
+
+        partial class Part1TableDataTable
+        {
+        }
+    }
+}
