b1091f3ac71fd0563aaeff4d410de696f3f30050
Evgenii Babak <evgenii.babak@gmail.com>
2022-10-27 14:46:07 +0600

    !!!!

---
+++ CASReports/Builders/CAA/SpecialistBuilder.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using Auxiliary;
+using CASReports.Datasets.CAA;
+using CASReports.ReportTemplates;
+using CASReports.ReportTemplates.CAA;
+using CASTerms;
+using SmartCore.Entities.General.Atlbs;
+using SmartCore.Entities.General.MaintenanceWorkscope;
+using SmartCore.Entities.General.Personnel;
+using ATLBDataSet = CASReports.Datasets.ATLBDataSet;
+
+namespace CASReports.Builders.CAA
+{
+    /// <summary>
+    /// ����������� ������ �� ��������� ������� ��
+    /// </summary>
+    public class SpecialistBuilder : AbstractReportBuilder
+    {
+	    private readonly Specialist _spec;
+	    
+	    public SpecialistBuilder(Specialist spec)
+	    {
+		    _spec = spec;
+	    }
+
+        #region Methods
+
+        #region public override object GenerateReport()
+        
+        public override object GenerateReport()
+        {
+	        CAASpecReportPart1 report = new CAASpecReportPart1();
+            report.SetDataSource(GenerateDataSet());
+            return report;
+        }
+
+        #endregion
+
+        #region private ATLBDataSet GenerateDataSet()
+
+        private SpecialistDataSet GenerateDataSet()
+        {
+	        SpecialistDataSet dataSet = new SpecialistDataSet();
+            AddSpecialistInformation(dataSet);
+            
+            return dataSet;
+        }
+
+        #endregion
+
+        #region private void AddATLBInformation(ATLBDataSet dataSet)
+
+        private void AddSpecialistInformation(SpecialistDataSet dataSet)
+        {
+	        dataSet.Part1Table.AddPart1TableRow();
+        }
+
+        #endregion
+        
+        #endregion
+    }
+}
--- CASReports/Datasets/CAA/SpecialistDataSet.Designer.cs
+++ CASReports/Datasets/CAA/SpecialistDataSet.Designer.cs
@@ -20,9 +20,9 @@ namespace CASReports.Datasets.CAA {
     [global::System.ComponentModel.DesignerCategoryAttribute("code")]
     [global::System.ComponentModel.ToolboxItem(true)]
     [global::System.Xml.Serialization.XmlSchemaProviderAttribute("GetTypedDataSetSchema")]
-    [global::System.Xml.Serialization.XmlRootAttribute("ATLBDataSet")]
+    [global::System.Xml.Serialization.XmlRootAttribute("SpecialistDataSet")]
     [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.DataSet")]
-    public partial class ATLBDataSet : global::System.Data.DataSet {
+    public partial class SpecialistDataSet : global::System.Data.DataSet {
         
         private Part1TableDataTable tablePart1Table;
         
@@ -30,7 +30,7 @@ namespace CASReports.Datasets.CAA {
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
-        public ATLBDataSet() {
+        public SpecialistDataSet() {
             this.BeginInit();
             this.InitClass();
             global::System.ComponentModel.CollectionChangeEventHandler schemaChangedHandler = new global::System.ComponentModel.CollectionChangeEventHandler(this.SchemaChanged);
@@ -41,7 +41,7 @@ namespace CASReports.Datasets.CAA {
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
-        protected ATLBDataSet(global::System.Runtime.Serialization.SerializationInfo info, global::System.Runtime.Serialization.StreamingContext context) : 
+        protected SpecialistDataSet(global::System.Runtime.Serialization.SerializationInfo info, global::System.Runtime.Serialization.StreamingContext context) : 
                 base(info, context, false) {
             if ((this.IsBinarySerialized(info, context) == true)) {
                 this.InitVars(false);
@@ -127,7 +127,7 @@ namespace CASReports.Datasets.CAA {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         public override global::System.Data.DataSet Clone() {
-            ATLBDataSet cln = ((ATLBDataSet)(base.Clone()));
+            SpecialistDataSet cln = ((SpecialistDataSet)(base.Clone()));
             cln.InitVars();
             cln.SchemaSerializationMode = this.SchemaSerializationMode;
             return cln;
@@ -225,7 +225,7 @@ namespace CASReports.Datasets.CAA {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         public static global::System.Xml.Schema.XmlSchemaComplexType GetTypedDataSetSchema(global::System.Xml.Schema.XmlSchemaSet xs) {
-            ATLBDataSet ds = new ATLBDataSet();
+            SpecialistDataSet ds = new SpecialistDataSet();
             global::System.Xml.Schema.XmlSchemaComplexType type = new global::System.Xml.Schema.XmlSchemaComplexType();
             global::System.Xml.Schema.XmlSchemaSequence sequence = new global::System.Xml.Schema.XmlSchemaSequence();
             global::System.Xml.Schema.XmlSchemaAny any = new global::System.Xml.Schema.XmlSchemaAny();
@@ -573,7 +573,7 @@ namespace CASReports.Datasets.CAA {
             public static global::System.Xml.Schema.XmlSchemaComplexType GetTypedTableSchema(global::System.Xml.Schema.XmlSchemaSet xs) {
                 global::System.Xml.Schema.XmlSchemaComplexType type = new global::System.Xml.Schema.XmlSchemaComplexType();
                 global::System.Xml.Schema.XmlSchemaSequence sequence = new global::System.Xml.Schema.XmlSchemaSequence();
-                ATLBDataSet ds = new ATLBDataSet();
+                SpecialistDataSet ds = new SpecialistDataSet();
                 global::System.Xml.Schema.XmlSchemaAny any1 = new global::System.Xml.Schema.XmlSchemaAny();
                 any1.Namespace = "http://www.w3.org/2001/XMLSchema";
                 any1.MinOccurs = new decimal(0);
