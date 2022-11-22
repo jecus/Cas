091cebe776dd6dede574983b596fbe210638a4d1
Evgenii Babak <evgenii.babak@gmail.com>
2022-11-22 21:51:58 +0600

    Med

--- CASReports/Builders/CAA/SpecialistMedicalBuilder.cs
+++ CASReports/Builders/CAA/SpecialistMedicalBuilder.cs
@@ -27,7 +27,7 @@ namespace CASReports.Builders.CAA
         public override object GenerateReport()
         {
 	        CAASpecMedical report = new CAASpecMedical();
-            //report.SetDataSource(GenerateDataSet());
+            report.SetDataSource(GenerateDataSet());
             return report;
         }
         
@@ -36,7 +36,50 @@ namespace CASReports.Builders.CAA
         private SpecialistDataSet GenerateDataSet()
         {
 	        SpecialistDataSet dataSet = new SpecialistDataSet();
+	        
+	        AddSpecialistInformation(dataSet);
+	        AddAdditionalInformation(dataSet);
 	        return dataSet;
+        }
+        
+        private void AddAdditionalInformation(SpecialistDataSet dataSet)
+        {
+	        var caa = GlobalObjects.CaaEnvironment.Operators.FirstOrDefault();
+	        dataSet.AdditionalInfo.AddAdditionalInfoRow(caa.Name, caa.Settings?.ShortName ?? "",_reporter.FirstName, _reporter.LastName, _reporter.Images.Sign, caa.Stamp);
+        }
+        
+        
+        private void AddSpecialistInformation(SpecialistDataSet dataSet)
+        {
+	        var _license = _spec.Licenses.FirstOrDefault();
+	        var licenseCaa = _license.CaaLicense.FirstOrDefault(c => c.CaaType == CaaType.Licence);
+	        
+	        
+	        var licenceNumber = licenseCaa?.CAANumber ?? String.Empty;
+	        var titleLicense = _license?.EmployeeLicenceType.ShortName ?? String.Empty;
+	        var name = $"{_spec.LastName} {_spec.FirstName}";
+	        var dateOfBirth = $"{_spec.DateOfBirth:dd.MM.yyyy}";
+	        var placeOfBirth = _spec.Nationality;
+	        var adress = _spec.Address;
+	        var nationality = $"{_spec.Citizenship.FullName} ({_spec.Citizenship.ShortName})";
+	        var issuing = $"{_license?.IssueDate:dd.MM.yyyy}" ?? String.Empty;
+	        var valid =  _license.IsValidTo ?   $"{_license?.ValidToDate:dd.MM.yyyy}" : "unlimited";
+	        var countryCode = licenseCaa?.Caa.ShortName ?? "";
+	        var privilages = "Unlimited";
+
+	        if (_spec.LicenseRemark.Any(i => i.LicenseRestriction.FullName.Contains("Radiotelephony operation in English")))
+		        privilages = "This holder of this Licence demonstrated to operate R/T equipment on board aircraft in English";
+
+	        var level = "";
+
+	        dataSet.Part1Table
+		        .AddPart1TableRow(_spec.Images.Sign, licenceNumber,name,dateOfBirth,placeOfBirth
+			        ,adress,nationality,issuing,null,valid,countryCode,titleLicense,privilages, level,
+			        $"{DateTime.Now:dd.MM.yyyy HH:mm tt}",_spec.Images.QR
+			        , _spec.Images.Photo);
+
+
+	        
         }
     }
 }
